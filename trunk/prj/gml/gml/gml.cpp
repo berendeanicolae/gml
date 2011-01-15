3c6f313518df81b06d34090d77bd27042c731f8c
imcimpoesu@gmail.com <imcimpoesu@gmail.com@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-01-15 12:09:28 +0000

    first look at the project

---
+++ trunk/prj/gml/gml/gml.cpp
@@ -0,0 +1,13 @@
+// gml.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Compat.h"
+
+#include <vector>
+
+Int32 _tmain(Int32 argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
