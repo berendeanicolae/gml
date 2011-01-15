1a684f52eb2328b165f95e27f039e9cd7438e15a
imcimpoesu@gmail.com <imcimpoesu@gmail.com@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-01-15 16:13:11 +0000

    added gml.h

---
+++ trunk/inc/gml.h
@@ -0,0 +1,13 @@
+#ifndef __GML_H
+#define __GML_H
+
+#include "LocalDefines.h"
+#include "Compat.h"
+#include "GTVector.h"
+
+#include "MLInterface.h"
+
+#include "ConsoleNotifier.h"
+#include "XorElemsDatabase.h"
+
+#endif
