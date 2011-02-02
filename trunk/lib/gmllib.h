79ca43477e14f839ed550d63e9883749e19608ca
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-02 20:30:18 +0000

    Added INotify for strings

--- trunk/lib/gmllib.h
+++ trunk/lib/gmllib.h
@@ -848,9 +848,9 @@ namespace GML
 		public:
 			virtual bool	Init(void *initData) = 0;
 			virtual bool	Uninit() = 0;
-			virtual void	Notify(UInt32 messageID,void *Data,UInt32 DataSize) = 0;
+			virtual bool	Notify(UInt32 messageID,void *Data,UInt32 DataSize) = 0;
 
-			void			NotifyString(char* format,...);
+			bool			NotifyString(UInt32 messageID,char* format,...);
 		};
 	}
 }
