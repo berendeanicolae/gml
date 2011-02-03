7fa0176099c0f11b0dfb5592936e129ed5418324
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-03 14:07:17 +0000

    Double si Float

--- trunk/code/GmlLib/inc/DBRecord.h
+++ trunk/code/GmlLib/inc/DBRecord.h
@@ -35,6 +35,8 @@ namespace GML
 				UInt64		UInt64Val;
 				void*		RawPtrVal;
 				UInt8*		BytesVal;
+				double		DoubleVal;
+				float		FloatVal;
 				char*		AsciiStrVal;
 				wchar_t*	UnicStrVal;
 				RecordHash	Hash;
