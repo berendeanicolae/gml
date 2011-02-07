6c693338cda1199c61d2a20f9147fb0f871599b4
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-07 06:55:38 +0000

    IConector constructor

--- lib/gmllib.h
+++ lib/gmllib.h
@@ -1203,6 +1203,7 @@ namespace GML
 	public:
 		static GML::Utils::INotify*		CreateNotifyer(char *pluginName,void *objectData = NULL);	
 		static GML::DB::IDataBase*		CreateDataBase(char *pluginName,GML::Utils::INotify &notify,char *connectionString);
+		static GML::ML::IConector*		CreateConectors(char *conectorsList,GML::Utils::INotify &notify,GML::DB::IDataBase &database);
 	};
 }
 
