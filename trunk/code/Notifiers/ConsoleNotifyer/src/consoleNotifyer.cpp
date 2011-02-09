eed344fc3ea45ef3c51bbbdf36c19b2d73ed9db9
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-09 01:33:52 +0000

    Move ConsoleNotifyer

---
+++ trunk/code/Notifiers/ConsoleNotifyer/src/consoleNotifyer.cpp
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "ConsoleNotifyer.h"
+
+
+bool ConsoleNotifyer::Init(void *initData)
+{
+	return true;
+}
+bool ConsoleNotifyer::Uninit()
+{
+	return true;
+}
+bool ConsoleNotifyer::Notify(UInt32 messageID,void *Data,UInt32 DataSize)
+{
+	char *text = (char *)Data;
+	printf("%4d => ",messageID);
+	if ((Data!=NULL) && (DataSize>0))
+	{
+		for (UInt32 tr =0;tr<DataSize;tr++)
+		{
+			if ((text[tr]>=' ') && (text[tr]<=128))
+				printf("%c",text[tr]);
+			else
+				printf("{\\x%02X}",(unsigned char)text[tr]);
+		}
+	}
+	printf("\n");
+	return true;
+}
