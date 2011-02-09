666adbf6b6b9cce1514d86630440cb90803c3888
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-09 01:33:52 +0000

    Move ConsoleNotifyer

---
+++ code/Notifiers/ConsoleNotifyer/inc/ConsoleNotifyer.h
@@ -0,0 +1,15 @@
+#ifndef __CONSOLE__NOTIFYER__
+#define __CONSOLE__NOTIFYER__
+
+#include "gmllib.h"
+
+class ConsoleNotifyer: public GML::Utils::INotify
+{
+public:
+	bool	Init(void *initData);
+	bool	Uninit();
+	bool	Notify(UInt32 messageID,void *Data,UInt32 DataSize);
+};
+ 
+
+#endif
