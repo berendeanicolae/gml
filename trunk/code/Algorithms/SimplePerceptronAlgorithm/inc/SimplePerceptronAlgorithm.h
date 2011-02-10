9ff5a0261bcbaa55e908779d4b2eedcf0dbda13d
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-10 13:50:31 +0000

    SimplePerceptronAlgorithm added

---
+++ trunk/code/Algorithms/SimplePerceptronAlgorithm/inc/SimplePerceptronAlgorithm.h
@@ -0,0 +1,11 @@
+#ifndef __SIMPLE_PERCEPTRON_ALG__
+#define __SIMPLE_PERCEPTRON_ALG__
+
+#include "GMLLib.h"
+
+class SimplePerceptronAlgorithm: public GML::
+{
+};
+
+
+#endif
---
+++ trunk/code/Algorithms/SimplePerceptronAlgorithm/src/SimplePerceptronAlgorithm.cpp
@@ -0,0 +1,3 @@
+#include "SimplePerceptronAlgorithm.h"
+
+
