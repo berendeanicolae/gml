14770c48be881f41e7adc2a162d0736780f231fc
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-10 14:34:43 +0000

    Added a SimplePerceptronAlgorithm

--- trunk/code/Algorithms/SimplePerceptronAlgorithm/inc/SimplePerceptronAlgorithm.h
+++ trunk/code/Algorithms/SimplePerceptronAlgorithm/inc/SimplePerceptronAlgorithm.h
@@ -3,8 +3,29 @@
 
 #include "GMLLib.h"
 
-class SimplePerceptronAlgorithm: public GML::
+class SimplePerceptronAlgorithm: public GML::Algorithm::IAlgorithm
 {
+	GML::Utils::INotify		*notif;
+	GML::DB::IDataBase		*db;
+	GML::ML::IConector		*con;
+	GML::ML::MLRecord		rec;
+	double					*weight;
+
+
+	GML::Utils::GString		dbName,conString,conectorString,notifString;
+	double					learningRate;
+	UInt32					maxIteratii;
+
+protected:
+	void	Train();
+	void	Test();
+
+public:
+	SimplePerceptronAlgorithm();
+	bool	SetConfiguration(GML::Utils::AttributeList &config);
+	bool	GetConfiguration(GML::Utils::AttributeList &config);
+	bool	Init();
+	void	Execute(UInt32 command);
 };
 
 
