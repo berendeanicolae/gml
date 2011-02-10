2851a90bbac1d3a4a074e19f2c2290abb0534604
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-10 14:38:10 +0000

    Shows message=100 (Alorithm Results)

--- trunk/code/Notifiers/ConsoleNotifyer/src/consoleNotifyer.cpp
+++ trunk/code/Notifiers/ConsoleNotifyer/src/consoleNotifyer.cpp
@@ -14,6 +14,13 @@ bool ConsoleNotifyer::Notify(UInt32 messageID,void *Data,UInt32 DataSize)
 {
 	char *text = (char *)Data;
 	printf("%4d => ",messageID);
+	// pentru AlgResult
+	if (messageID==100)
+	{
+		GML::Utils::AlgorithmResult	*res = (GML::Utils::AlgorithmResult *)Data;
+		printf("TP:%5d |TN:%5d |FN:%5d |FP:%5d |Se:%3.2lf|Sp:%3.2lf|Acc:%3.2lf|\n",res->tp,res->tn,res->fn,res->fp,res->se,res->sp,res->acc);
+		return true;
+	}
 	if ((Data!=NULL) && (DataSize>0))
 	{
 		for (UInt32 tr =0;tr<DataSize;tr++)
