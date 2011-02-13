576d101a76e9971d0379de651ddc767d4463a151
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-13 13:34:34 +0000

    Delete TestGmlLib

---
+++ prj/gml/Test/Test.cpp
@@ -0,0 +1,37 @@
+// TestGmlLib.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "gmllib.h"
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	GML::Utils::AttributeList	attr;
+	GML::Utils::GString			str;
+
+	attr.Clear();
+	attr.Create("dbPath=E:\\mcu.db");
+	attr.UpdateString("dbPath",str);
+
+	attr.AddString("DbName","SimpleTextFileDB");
+	attr.AddString("DbConnString","FileName=E:\\lucru\\GML\\gml\\bin\\small_mcu.txt.text-plugin");  
+	attr.AddString("Conector","FullCacheConnector");
+	attr.AddString("Notifyer","ConsoleNotifyer");
+	attr.AddDouble("LearningRate",0.01);
+	attr.AddUInt32("MaxIteratii",1000);
+
+	GML::Algorithm::IAlgorithm *alg = GML::Builder::CreateAlgorithm("SimplePerceptronAlgorithm","");
+	if (alg==NULL)
+		return 1;
+	if (alg->SetConfiguration(attr)==false)
+		return 1;
+	if (alg->Init()==false)
+		return 1;
+	
+	alg->Execute(0);
+
+	return 0;
+}
+
