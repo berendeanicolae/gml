92f0b0592c77760c57d535abd29517d8693c7698
PCManticore <PCManticore@gmail.com>
2011-02-18 16:37:21 +0000

    Ported build script to 3.1 ( mcu request).

--- tools/build_gmllib.py
+++ tools/build_gmllib.py
@@ -26,15 +26,15 @@ list_headers = [
 c_path = sys.argv[1]
 f_path = sys.argv[2]
 
-print "Files:",c_path
-print "Dest :",f_path
-f = open(os.path.join(f_path,"gmllib.h"),"wb")
+print("Files:",c_path)
+print("Dest :",f_path)
+f = open(os.path.join(f_path,"gmllib.h"),"w")
 f.write("#ifndef __GML_LIB__\n#define __GML_LIB__\n\n")
-f.write(open(os.path.join(c_path,"compat.h"),"rb").read())
+f.write(open(os.path.join(c_path,"compat.h"),"r").read())
 f.write("\n\n//-----------------------------------------------------------------------------------------------------------------------\n\n")
 #f.write("namespace GML {\n");
 for name in list_headers:
-	print "Adding ... "+name
+	print("Adding ... "+name)
 	f.write("//===================== "+name+" =================================================================================\n");
 	for line in open(os.path.join(c_path,name),"rb"):
 		if "#include" in line:
