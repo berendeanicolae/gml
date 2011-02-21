75d0051afe2202558dfc62600be9d3fa26fb1d24
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-21 19:52:34 +0000

    Attributes for DataBase Builder

--- trunk/lib/gmllib.h
+++ trunk/lib/gmllib.h
@@ -1527,7 +1527,7 @@ namespace GML
 	{
 	public:
 		static GML::Utils::INotify*			CreateNotifyer(char *pluginName);	
-		static GML::DB::IDataBase*			CreateDataBase(char *pluginName,GML::Utils::INotify &notify,char *connectionString);
+		static GML::DB::IDataBase*			CreateDataBase(char *pluginName,GML::Utils::INotify &notify);
 		static GML::ML::IConector*			CreateConectors(char *conectorsList,GML::Utils::INotify &notify,GML::DB::IDataBase &database);
 		static GML::Algorithm::IAlgorithm*	CreateAlgorithm(char *algorithmLib,char *algorithmName=NULL);
 		static char*						GetAlgorithmList(char *algorithmLib);
