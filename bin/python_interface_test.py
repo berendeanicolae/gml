af191dbedf5ccf2fdb507afc03672d0f934b61f5
imcimpoesu <imcimpoesu@gmail.com>
2011-02-23 16:51:59 +0000

    added Set function in IAlgorithm (python interface)

--- bin/python_interface_test.py
+++ bin/python_interface_test.py
@@ -5,23 +5,21 @@ if not len(sys.argv) > 1:
 	sys.exit()
 path = sys.argv[1]
 
-attr = gml.AttributeList()
-
-attr.Set({ "DbName":"SimpleTextFileDB{FileName=%s}"%path,
-	   "Conector":"BitConnector{Table=RecordTable}",
-	   "Notifyer":"ConsoleNotifyer{useColors=True}",
-	   "LearningRate":0.02,
-	   "MaxIteratii":100})
-
 b = gml.Builder()
 
 ialg = b.CreateAlgorithm("SimplePerceptronAlgorithm")
 print("Inited algorithm: {}".format(ialg)) 
+
 if not ialg:
 	print ("could not init the algorithm")
 	sys.exit()
 
-config = ialg.SetProperty(attr)
+config = ialg.Set({"DbName":"SimpleTextFileDB{FileName=%s}"%path,
+		   "Conector":"BitConnector{Table=RecordTable}",
+		   "Notifyer":"ConsoleNotifyer{useColors=True}",
+		   "LearningRate":0.02,
+		   "MaxIteratii":100})
+
 if config:
 	_in = ialg.Init()
 	print("Result of algorithm initialization: %d"%_in)
