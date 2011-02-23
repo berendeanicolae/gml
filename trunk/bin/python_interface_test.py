3f0c1b15599f0d4c77b936ed49a74a47d2d2fc7f
imcimpoesu@gmail.com <imcimpoesu@gmail.com@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-23 14:00:55 +0000

    python interface test sample

---
+++ trunk/bin/python_interface_test.py
@@ -0,0 +1,43 @@
+import attributelist
+import ialgorithm
+import builder
+import sys
+if not len(sys.argv) > 1:
+	print("You must pass the path of database!")
+	sys.exit()
+path = sys.argv[1]
+
+attr = attributelist.AttributeList()
+
+attr.Set({ "DbName":"SimpleTextFileDB",
+	   "DbConnString":"FileName={}".format(path),	
+	   "Conector":"BitConnector{Table=RecordTable}",
+	   "Notifyer":"ConsoleNotifyer",
+	   "LearningRate":0.02,
+	   "MaxIteratii":100})
+
+b = builder.Builder()
+
+ialg = b.CreateAlgorithm("SimplePerceptronAlgorithm","")
+print("Inited algorithm: {}".format(ialg)) 
+if not ialg:
+	pass
+else:
+	config = ialg.SetConfiguration(attr)
+	if config:
+		_in = ialg.Init()
+		print("Result of algorithm initialization: %d"%_in)
+		if _in:
+			ialg.Execute("train");
+			ialg.Wait();
+
+
+
+
+
+
+
+
+
+
+
