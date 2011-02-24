688ad312061502429ed896d34028725fd7f56e22
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-24 06:07:52 +0000

    Rosemblatt Perceptron

---
+++ code/Algorithms/GDTPerceptrons/inc/RosenblattPerceptron.h
@@ -0,0 +1,14 @@
+#ifndef __Rosenblatt__Perceptron__
+#define __Rosenblatt__Perceptron__
+
+#include "GenericPerceptron.h"
+
+class RosenblattPerceptron: public GenericPerceptron
+{
+public:
+
+};
+
+
+#endif
+
