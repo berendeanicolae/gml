a6505a5178683764964c052154b03612047823c0
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-02-28 06:51:15 +0000

    BatchPerceptron

---
+++ trunk/prj/gml/BatchPerceptron/dllmain.cpp
@@ -0,0 +1,5 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+
+#include "BatchPerceptron.h"
+
+LIB_INTERFACE(BatchPerceptron,"GDT",1,"A simple BatchPerceptron with Multi-Thread support");
