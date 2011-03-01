82b3f430bfb76b34ced279f1d8ecb513b8f8960a
imcimpoesu@gmail.com <imcimpoesu@gmail.com@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-03-01 07:04:49 +0000

    first intermediate connector, the SplitConnector still untested due to some GDT bugs

---
+++ trunk/prj/TestGmlLib/TestGmlLib/TestGmlLib.cpp
@@ -0,0 +1,56 @@
+// TestGmlLib.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "gmllib.h"
+
+
+void Test(GML::Utils::GTVector<GML::DB::DBRecord> &VectPtr)
+{
+	GML::DB::DBRecord	rec;
+
+	for (int tr=0;tr<6;tr++)
+		VectPtr.PushByRef(rec);
+	for (int tr=0;tr<6;tr++)
+	{
+		VectPtr[tr].DoubleVal = tr;
+		VectPtr[tr].Name = "Hi";
+	}
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	GML::Utils::GTVector<GML::DB::DBRecord> VectPtr;
+
+	Test(VectPtr);
+	VectPtr.DeleteAll();
+
+
+	GML::Utils::AttributeList	attr;
+	GML::Utils::GString			str;
+
+	attr.Clear();
+	attr.Create("dbPath=E:\\mcu.db");
+	attr.UpdateString("dbPath",str);
+
+	attr.AddString("DbName","SimpleTextFileDB");
+	attr.AddString("DbConnString","FileName=E:\\lucru\\GML\\gml\\bin\\a.txt");  
+	attr.AddString("Conector","FullCacheConnector");
+	attr.AddString("Notifyer","ConsoleNotifyer");
+	attr.AddDouble("LearningRate",0.01);
+	attr.AddUInt32("MaxIteratii",100);
+
+	GML::Algorithm::IAlgorithm *alg = GML::Builder::CreateAlgorithm("SimplePerceptronAlgorithm","");
+	if (alg==NULL)
+		return 1;
+	if (alg->SetConfiguration(attr)==false)
+		return 1;
+	if (alg->Init()==false)
+		return 1;
+	alg->Execute(0);
+
+	return 0;
+}
+
