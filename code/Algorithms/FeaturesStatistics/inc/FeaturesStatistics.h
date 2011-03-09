d5b134c82dbf9bbc7b5e31036a70c46c086299ca
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-03-09 07:23:46 +0000

    FeaturesStatistics works :)

--- code/Algorithms/FeaturesStatistics/inc/FeaturesStatistics.h
+++ code/Algorithms/FeaturesStatistics/inc/FeaturesStatistics.h
@@ -1,7 +1,43 @@
 #include "gmllib.h"
 
-class FeaturesStatistics: publi GML::I
+struct FeaturesInfo
 {
+	UInt32 PozitiveCount;
+	UInt32 NegativeCount;
+};
+struct FeaturesThreadData
+{
+	GML::ML::MLRecord				Record;
+	GML::Utils::Interval			Range;
+	FeaturesInfo					*FI;
+public:
+	FeaturesThreadData() { FI=NULL; }
+};
+class FeaturesStatistics: public GML::Algorithm::IAlgorithm
+{
+	GML::DB::IDataBase				*db;
+	GML::ML::IConnector				*con;
+	GML::Utils::ThreadParalelUnit	*tpu;
+	FeaturesThreadData				All;
+public:
+	FeaturesThreadData				*fData;
+private:
+	// proprietati
+	GML::Utils::GString				Conector;
+	GML::Utils::GString				DataBase;
+	GML::Utils::GString				Notifier;
+
+	UInt32							threadsCount;
+
+	bool							CreateFeaturesInfo(FeaturesThreadData *fInfo);
+	bool							Compute();
+	void							PrintStats();
+public:
+	FeaturesStatistics();
+
+	void							OnRunThreadCommand(FeaturesThreadData &ftd,UInt32 command);
+	bool							Init();
+	void							OnExecute(char *command);
 };
 
 
--- code/Algorithms/FeaturesStatistics/src/FeaturesStatistics.cpp
+++ code/Algorithms/FeaturesStatistics/src/FeaturesStatistics.cpp
@@ -1,3 +1,170 @@
 #include "FeaturesStatistics.h"
 
+void ThreadRedirectFunction(GML::Utils::IParalelUnit *paralel,void *context)
+{
+	FeaturesStatistics *fs = (FeaturesStatistics *)context;
+	fs->OnRunThreadCommand(fs->fData[paralel->GetID()],paralel->GetCodeID());
+}
+//====================================================================================================
+FeaturesStatistics::FeaturesStatistics()
+{
+	ObjectName = "FeaturesStatistics";
 
+	LinkPropertyToString("DataBase"					,DataBase				,"");
+	LinkPropertyToString("Connector"				,Conector				,"");
+	LinkPropertyToString("Notifier"					,Notifier				,"");
+	LinkPropertyToUInt32("ThreadsCount"				,threadsCount			,1);
+}
+bool FeaturesStatistics::CreateFeaturesInfo(FeaturesThreadData *fInfo)
+{
+	UInt32	tr;
+	if (con->CreateMlRecord(fInfo->Record)==false)
+	{
+		notif->Error("[%s] -> Unable to create MLRecord",ObjectName);
+		return false;
+	}
+
+	if ((fInfo->FI = new FeaturesInfo[con->GetFeatureCount()])==NULL)
+	{
+		notif->Error("[%s] -> Unable to allocate space for FeaturesInfo (%d)",ObjectName,con->GetFeatureCount());
+		return false;
+	}
+	for (tr=0;tr<con->GetFeatureCount();tr++)
+	{
+		fInfo->FI[tr].NegativeCount = 0;
+		fInfo->FI[tr].PozitiveCount = 0;
+	}
+
+	return true;
+}
+bool FeaturesStatistics::Init()
+{
+	UInt32		tr,rap;
+
+	// creez obiectele:
+	if ((notif = GML::Builder::CreateNotifier(Notifier.GetText()))==NULL)
+		return false;
+	if ((db = GML::Builder::CreateDataBase(DataBase.GetText(),*notif))==NULL)
+	{
+		notif->Error("[%s] -> Unable to create Database (%s)",ObjectName,DataBase.GetText());
+		return false;
+	}
+	if (db->Connect()==false)
+	{
+		notif->Error("[%s] -> Unable to connesct to Database (%s)",ObjectName,DataBase.GetText());
+		return false;
+	}
+	if ((con = GML::Builder::CreateConnectors(Conector.GetText(),*notif,*db))==NULL)
+	{
+		notif->Error("[%s] -> Unable to create Conector (%s)",ObjectName,Conector.GetText());
+		return false;
+	}
+	if ((fData = new FeaturesThreadData[threadsCount])==NULL)
+	{
+		notif->Error("[%s] -> Unable to create %d FeaturesThreadData ",ObjectName,threadsCount);
+		return false;
+	}
+	if (CreateFeaturesInfo(&All)==false)
+		return false;
+	// pornesc threadPool-ul
+	if ((tpu = new GML::Utils::ThreadParalelUnit[threadsCount])==NULL)
+	{
+		notif->Error("[%s] -> Unable to create %d threads ",ObjectName,threadsCount);
+		return false;
+	}
+	for (tr=0;tr<threadsCount;tr++)
+	{
+		if (tpu[tr].Init(tr,this,ThreadRedirectFunction)==false)
+		{
+			notif->Error("[%s] -> Unable to start thread #%d",ObjectName,tr);
+			return false;
+		}
+		if (CreateFeaturesInfo(&fData[tr])==false)
+			return false;
+	}
+	// splituim
+	rap = (con->GetRecordCount()/threadsCount)+1;
+
+	// aloc memorie pentru indexi
+	for (tr=0;tr<threadsCount;tr++)
+	{
+		if (tr+1==threadsCount)
+			fData[tr].Range.Set(tr*rap,con->GetRecordCount());
+		else
+			fData[tr].Range.Set(tr*rap,(tr+1)*rap);
+	}
+	return true;
+}
+void FeaturesStatistics::OnRunThreadCommand(FeaturesThreadData &ftd,UInt32 command)
+{
+	UInt32	tr,gr,count;
+
+	count = con->GetFeatureCount();
+	// citesc datele asociate range-ului
+	for (tr=ftd.Range.Start;(tr<ftd.Range.End) && (StopAlgorithm==false);tr++)
+	{
+		if (con->GetRecord(ftd.Record,tr)==false)
+		{
+			notif->Error("[%s] -> Unable to read record %d",ObjectName,tr);
+			return;
+		}
+		for (gr=0;(gr<count) && (StopAlgorithm==false);gr++)
+			if (ftd.Record.Features[gr]!=0)
+			{
+				if (ftd.Record.Label==1)
+					ftd.FI[gr].PozitiveCount++;
+				else
+					ftd.FI[gr].NegativeCount++;
+			}
+	}
+}
+void FeaturesStatistics::PrintStats()
+{
+	UInt32	tr;
+
+	for (tr=0;tr<con->GetFeatureCount();tr++)
+		notif->Info("%3d|Poz=%6d|Neg=%6d|",tr,All.FI[tr].PozitiveCount,All.FI[tr].NegativeCount);
+
+}
+bool FeaturesStatistics::Compute()
+{
+	UInt32	tr,gr;
+
+	notif->Info("[%s] -> Computing statistics ...",ObjectName);
+	// executie
+	for (tr=0;tr<threadsCount;tr++)
+		if (tpu[tr].Execute(1)==false)
+		{
+			notif->Error("Error on runnig thread #%d",tr);
+			return false;
+		}
+	// asteptare
+	for (tr=0;tr<threadsCount;tr++)
+		if (tpu[tr].WaitToFinish()==false)
+		{
+			notif->Error("WaitToFinish failed on thread #%d",tr);
+			return false;
+		}
+	// fac aditia
+	for (tr=0;tr<threadsCount;tr++)
+	{
+		for (gr=0;gr<con->GetFeatureCount();gr++)
+		{
+			All.FI[gr].PozitiveCount+=fData[tr].FI[gr].PozitiveCount;
+			All.FI[gr].NegativeCount+=fData[tr].FI[gr].NegativeCount;
+		}
+	}
+	notif->Info("[%s] -> DataBase processed ok",ObjectName);
+	PrintStats();
+	return true;
+}
+void FeaturesStatistics::OnExecute(char *command)
+{
+	StopAlgorithm = false;
+	if (GML::Utils::GString::Equals(command,"compute",true))
+	{
+		Compute();
+		return;
+	}
+	notif->Error("Unkwnown command: %s",command);
+}
