4479479ce8df886baa86d3e6bbf74552b5621b08
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-03-19 18:07:50 +0000

    MapConector works :)

---
+++ code/Connectors/MapConnector/src/MapConnector.cpp
@@ -0,0 +1,135 @@
+#include "MapConnector.h"
+
+MapConnector::MapConnector()
+{
+	ObjectName = "MapConnector";
+
+	LinkPropertyToUInt32("MapMethod",mapMethod,UseAND,	"!!LIST:UseAND=0,UseOR,UseXOR,UseAnd+Or!!\n"
+														"Selects the method to be used for mapping\n"
+														"* UseAND -> feat(1..n) will become feat`(1..m) where\n"														
+														"\n"
+														);
+}
+
+bool	MapConnector::OnInit()
+{
+	featuresCount = 0;
+	if (conector==NULL)
+	{
+		notifier->Error("[%s] -> Link connector not set",ObjectName);
+		return false;
+	}
+	// calculez cate feature-uri o sa am in functie de metoda
+	switch (mapMethod)
+	{
+		case UseAND:
+		case UseXOR:
+		case UseOR:
+			featuresCount = (conector->GetFeatureCount() * (conector->GetFeatureCount()+1))>>1;
+			break;
+		case UseAnd_Or:
+			featuresCount = (conector->GetFeatureCount() * (conector->GetFeatureCount()+1));
+			break;
+		default:
+			notifier->Error("[%s] -> Unknown method ID = %d",ObjectName,mapMethod);
+			return false;
+	};
+	notifier->Info("[%s] -> Mapping %d features to %d features ",ObjectName,conector->GetFeatureCount(),featuresCount);
+	return true;
+}
+bool	MapConnector::Close()
+{
+	return true;
+}
+bool	MapConnector::SetRecordInterval(UInt32 start, UInt32 end)
+{
+	return true;
+}
+bool	MapConnector::CreateMlRecord (GML::ML::MLRecord &record)
+{
+	if ((record.Parent = new GML::ML::MLRecord())==NULL)
+	{
+		notifier->Error("[%s] -> Unable to create MLRecord ",ObjectName);
+		return false;
+	}
+	if (conector->CreateMlRecord(*record.Parent)==false)
+	{
+		notifier->Error("[%s] -> Unable to create MLRecord from parent",ObjectName);
+		return false;
+	}
+	record.FeatCount = featuresCount;
+	return ((record.Features = new double[featuresCount])!=NULL);
+}
+bool	MapConnector::GetRecord(GML::ML::MLRecord &record,UInt32 index)
+{
+	UInt32		tr,gr,pCount;
+	double		*p1,*p2,*pMap;
+
+	if (conector->GetRecord(*record.Parent,index)==false)
+		return false;
+	pCount = conector->GetFeatureCount();
+	record.Weight = record.Parent->Weight;
+	record.Label = record.Parent->Label;
+	record.FeatCount = featuresCount;
+
+	pMap = record.Features;
+	switch (mapMethod)
+	{
+		case UseAND:
+			for (tr=0,p1=record.Parent->Features;tr<pCount;tr++,p1++)
+				for (gr=tr,p2=p1;gr<pCount;gr++,p2++,pMap++)
+					(*pMap) = (double)((UInt32)(*p1) & (UInt32)(*p2));
+			break;
+		case UseOR:
+			for (tr=0,p1=record.Parent->Features;tr<pCount;tr++,p1++)
+				for (gr=tr,p2=p1;gr<pCount;gr++,p2++,pMap++)
+					(*pMap) = (double)((UInt32)(*p1) | (UInt32)(*p2));
+			break;
+		case UseXOR:
+			for (tr=0,p1=record.Parent->Features;tr<pCount;tr++,p1++)
+				for (gr=tr,p2=p1;gr<pCount;gr++,p2++,pMap++)
+					(*pMap) = (double)((UInt32)(*p1) ^ (UInt32)(*p2));
+			break;
+		case UseAnd_Or:
+			for (tr=0,p1=record.Parent->Features;tr<pCount;tr++,p1++)
+				for (gr=tr,p2=p1;gr<pCount;gr++,p2++,pMap++)
+				{
+					(*pMap) = (double)((UInt32)(*p1) & (UInt32)(*p2));
+					pMap++;
+					(*pMap) = (double)((UInt32)(*p1) | (UInt32)(*p2));
+				}
+			break;
+		default:
+			notifier->Error("[%s] -> Unknown method ID = %d",ObjectName,mapMethod);
+			return false;
+	}
+
+	return true;
+}
+bool	MapConnector::GetRecordLabel(double &Label,UInt32 index)
+{
+	return conector->GetRecordLabel(Label,index);
+}
+bool	MapConnector::FreeMLRecord(GML::ML::MLRecord &record)
+{
+	if (record.Features!=NULL)
+	{
+		delete record.Features;
+		record.Features = NULL;
+	}
+	if (conector->FreeMLRecord(*record.Parent))
+		return false;
+	return true;
+}
+UInt32	MapConnector::GetFeatureCount()
+{
+	return featuresCount;
+}
+UInt32	MapConnector::GetRecordCount()
+{
+	return conector->GetRecordCount();
+}
+UInt32	MapConnector::GetTotalRecordCount()
+{
+	return conector->GetTotalRecordCount();
+}
