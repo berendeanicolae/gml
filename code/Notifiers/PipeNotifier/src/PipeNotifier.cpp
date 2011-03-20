be9bc29664d041fb11f6fc041432cea86b90209f
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-03-20 11:45:10 +0000

    PipeNotifier ready for testing :)

--- code/Notifiers/PipeNotifier/src/PipeNotifier.cpp
+++ code/Notifiers/PipeNotifier/src/PipeNotifier.cpp
@@ -1,62 +1,85 @@
 #include "stdio.h"
-#include "ConsoleNotifier.h"
+#include "PipeNotifier.h"
 
-ConsoleNotifier::ConsoleNotifier()
+PipeNotifier::PipeNotifier()
 {
-	ObjectName = "ConsoleNotifier";
-	LinkPropertyToBool("UseColors",useColors,false,"Specifies if colors should be used when showing mesages");
+	ObjectName = "PipeNotifier";
+
+	memset(&pi,0,sizeof(pi));
+	hPipe = INVALID_HANDLE_VALUE;
+
+	LinkPropertyToString("Client",Client,"","Specifies the name of the executable that will receive the pipe messages !");
 }
-void ConsoleNotifier::SetColor(unsigned char Fore, unsigned char Back)
-{
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),(Fore & 15)+(Back & 15)*16);
-}
-bool ConsoleNotifier::OnInit()
+
+bool PipeNotifier::OnInit()
 {	
-	tempStr.Create();
-	return true;
-}
-bool ConsoleNotifier::Uninit()
-{
-	return true;
-}
-bool ConsoleNotifier::Notify(UInt32 messageID,void *Data,UInt32 DataSize)
-{
-	char *text = (char *)Data;
-	SetColor(7,0);
-	printf("%4d => ",messageID);
-	// pentru AlgResult
-	if (messageID==100)
+    STARTUPINFO				si;
+	GML::Utils::GString		str;
+    
+	memset(&si,0,sizeof(si));
+	memset(&pi,0,sizeof(pi));
+	si.cb = sizeof(si);
+	si.dwFlags = STARTF_USESHOWWINDOW;
+	si.wShowWindow = SW_SHOW;
+
+	if (Client.Len()==0)
 	{
-		if (useColors)
-			SetColor(14,0);
-		GML::Utils::AlgorithmResult	*res = (GML::Utils::AlgorithmResult *)Data;	
-		printf("%4d|TP:%5d |TN:%5d |FN:%5d |FP:%5d |Se:%3.2lf|Sp:%3.2lf|Acc:%3.2lf|%s\n",(res->Iteration+1),(int)res->tp,(int)res->tn,(int)res->fn,(int)res->fp,res->se,res->sp,res->acc,res->time.GetPeriodAsString(tempStr));
-		SetColor(7,0);
+		DEBUGMSG("[%s] -> Client was not specified !",ObjectName);
+		return false;
+	}
+	if (!CreateProcess(Client.GetText(),NULL,NULL,NULL,FALSE,CREATE_SUSPENDED,NULL,NULL,&si,&pi))
+	{
+		DEBUGMSG("[%s] -> Unable to run %s !",ObjectName,Client.GetText());
+		memset(&pi,0,sizeof(pi));
+		return false;
+	}
+	
+	// creez pipe-ul
+	while (true)
+	{
+		if (str.SetFormated("\\\\.\\pipe\\GML_PIPE_%08X",pi.dwProcessId)==false)
+			break;
+		if ((hPipe = CreateNamedPipe(str.GetText(),PIPE_ACCESS_DUPLEX,PIPE_TYPE_MESSAGE|PIPE_READMODE_MESSAGE|PIPE_WAIT,1,2048,2048,0,NULL))==INVALID_HANDLE_VALUE)
+			break;
+		// dau drumul la process
+		ResumeThread(pi.hThread);
+		if (!ConnectNamedPipe(hPipe, NULL))
+			break;
+
 		return true;
 	}
-	if (useColors)
-	{
-		switch (messageID)
-		{
-			case GML::Utils::INotifier::NOTIFY_ERROR:
-				SetColor(12,0);
-				break;
-			case GML::Utils::INotifier::NOTIFY_INFO:
-				SetColor(11,0);
-				break;
-		};
-	}
-	if ((Data!=NULL) && (DataSize>0))
-	{
-		for (UInt32 tr =0;tr<DataSize;tr++)
-		{
-			if ((text[tr]>=' ') && (text[tr]<=128))
-				printf("%c",text[tr]);
-			else
-				printf("{\\x%02X}",(unsigned char)text[tr]);
-		}
-	}
-	printf("\n");
-	SetColor(7,0);
+	
+	if (hPipe!=INVALID_HANDLE_VALUE)
+		CloseHandle(hPipe);
+	DEBUGMSG("[%s] -> Unable to run/conect to %s !",ObjectName,Client.GetText());
+	TerminateProcess(pi.hProcess,1);
+	memset(&pi,0,sizeof(pi));
+	return false;
+}
+bool PipeNotifier::Uninit()
+{
+	if (pi.hProcess!=NULL)
+		TerminateProcess(pi.hProcess,0);
+	if (hPipe!=INVALID_HANDLE_VALUE)
+		CloseHandle(hPipe);	
+
+	memset(&pi,0,sizeof(pi));
+	hPipe = INVALID_HANDLE_VALUE;
 	return true;
+}
+bool PipeNotifier::Notify(UInt32 messageID,void *Data,UInt32 DataSize)
+{
+	DWORD	nrWrite;
+	if (hPipe!=INVALID_HANDLE_VALUE)
+	{
+		if ((WriteFile(hPipe,&messageID,sizeof(UInt32),&nrWrite,NULL)==FALSE) || (nrWrite!=sizeof(UInt32)))
+			return false;
+		if ((WriteFile(hPipe,&DataSize,sizeof(UInt32),&nrWrite,NULL)==FALSE) || (nrWrite!=sizeof(UInt32)))
+			return false;
+		if ((WriteFile(hPipe,Data,DataSize,&nrWrite,NULL)==FALSE) || (nrWrite!=DataSize))
+			return false;
+		return true;
+	}
+	
+	return false;
 }
