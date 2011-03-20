00d3d4d89b6eb5874d6557cae840f92969d66e3c
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-03-20 08:45:31 +0000

    StdDev and Average functions added

--- lib/gmllib.h
+++ lib/gmllib.h
@@ -1247,6 +1247,8 @@ namespace GML
 #define __VECTOR__OP__
 
 
+#define Mean	Average
+
 namespace GML
 {
 	namespace ML
@@ -1271,6 +1273,8 @@ namespace GML
 			static double	PointToPointDistance(double *p1,double *p2,UInt32 elements);
 			static double	PointToPointDistanceSquared(double *p1,double *p2,double *pWeight,UInt32 elements);
 			static double	PointToPointDistance(double *p1,double *p2,double *pWeight,UInt32 elements);
+			static double	Average(double *v,UInt32 elements);
+			static double	StandardDeviation(double *v,UInt32 elements);
 			
 		};
 	}
