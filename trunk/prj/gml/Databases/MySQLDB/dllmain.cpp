f8970afd06774c627fdd71cf4ba409976469e701
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-05 17:51:05 +0000

    Remove old IDataBAse Interface

--- trunk/prj/gml/Databases/MySQLDB/dllmain.cpp
+++ trunk/prj/gml/Databases/MySQLDB/dllmain.cpp
@@ -1,5 +1,5 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "GDTMySQL.h"
 
-LIB_INTERFACE(GDTMySQL,"HSC",1,"Interface for MySQL database");
+LIB_INTERFACE(GDTMySQL,"HSC & GDT",1,"Interface for MySQL database");
 
