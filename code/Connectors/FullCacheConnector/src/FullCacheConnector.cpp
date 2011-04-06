ae2b1a09f457aa2dff110acbaffc9608724ae6a7
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-06 21:37:41 +0000

    FullCacheConnector fixup

--- code/Connectors/FullCacheConnector/src/FullCacheConnector.cpp
+++ code/Connectors/FullCacheConnector/src/FullCacheConnector.cpp
@@ -1,272 +1,132 @@
-
 #include "FullCacheConnector.h"
 
 UInt32 FullCacheConnector::GetRecordCount()
 {
-	if (!Initialized) {
-		notifier->Error("connector not initialized");
-		return 0;
-	}
-
-	return RecordCount;
+	return nrRecords;
 }
 
 UInt32 FullCacheConnector::GetFeatureCount()
 {
-	if (!Initialized) {
-		notifier->Error("connector not initialized");
-		return 0;
-	}
-
 	return FeatureCount;		
 }
 
 bool FullCacheConnector::GetRecord( MLRecord &record,UInt32 index )
 {
-	if (index < IntervalStart || index >= IntervalEnd) {
-		notifier->Error("index is out of range, it has to be in interval (%d - %d)", IntervalStart, IntervalEnd);
+	if (index >= nrRecords) 
+	{
+		notifier->Error("[%s] -> index is out of range, it has to be less than %d", nrRecords);
 		return false;
 	}
-
-	if (RecordCount<=index) {
-		notifier->Error("an interval error occured: RecordsCache count(%d) is smaller than the provided index(%d)", RecordCount, index);
-		return false;
-	}
-
-	record.Features = (double*) &FeatureCache[index*FeatureCount]; 
+	record.Features = (double*) &Records[index*FeatureCount]; 
 	record.FeatCount = FeatureCount;
-	record.Label = LabelStorage[index];	
+	record.Label = Labels[index];	
 	
 	return true;
 }
 
 bool FullCacheConnector::CreateMlRecord( MLRecord &record )
 {	
-	if (!Initialized) {
-		notifier->Error("connector not initialized");
-		return false;
-	}	
-
 	record.FeatCount = FeatureCount;
-
 	return true;
 }
 
 bool FullCacheConnector::SetRecordInterval( UInt32 start, UInt32 end )
 {
-	if (start >= RecordCount || end >= RecordCount) {
-		notifier->Error("could not set interval margin above value %d", RecordCount);
-		return false;
-	}
-
-	if (end < start) {
-		notifier->Error("interval end margin could not be smaller than start margin");
-		return false;
-	}
-
-	IntervalStart = start;
-	IntervalEnd   = end;
-
-	return true;
+	return false;
 }
 
 bool FullCacheConnector::OnInit()
 {
-	GTFVector<DBRecord> VectPtr;
-	DBRecord* rec;
+	UInt32										tr,gr;
+	GML::Utils::GTFVector<GML::DB::DBRecord>	VectPtr;
+	double										*cPoz;
+	GML::Utils::GString							tempStr;
+	double										cValue;
 
-	UInt32	LabelPos, vectSize;
-	double  * FeaturesPtr;
-
-	notifier->Info("FullCacheConnector loading data");	
-	
-	char SqlString [MAX_SQL_QUERY_SIZE];
-
-	if (VectPtr.Create(1024)==false)
+	// daca iau datele din cache
+	if ((database==NULL) && (conector==NULL))
+		return true;
+	if (database==NULL)
 	{
-		notifier->Error("Unable to preallocate memory for records !");
+		notifier->Error("[%s] works with a filedata or a database",ObjectName);
 		return false;
 	}
-
-	// connect to database
-	if (!database->Connect()) 
+	if (database->Connect()==false)
 	{
-		notifier->Error("could not connect to database");
+		notifier->Error("[%s] -> Could not connect to database",ObjectName);
 		return false;
 	}
-
-	// build the internal cache 
-	
-	if (SelectQuery.Equals("*"))
-		sprintf_s(SqlString, MAX_SQL_QUERY_SIZE, "select * from %s", TableName);
-	else
-		sprintf_s(SqlString, MAX_SQL_QUERY_SIZE, "%s", SelectQuery.GetText());
-	
-	RecordCount = database->Select(SqlString);
-
-	// something bad has happend
-	if (RecordCount==0) 
+	tempStr.SetFormated("%s LIMIT 1",Query.GetText());
+	if (UpdateColumnInformations(tempStr.GetText())==false)
+		return false;
+	if (QueryRecordsCount(CountQuery.GetText(),nrRecords)==false)
+		return false;
+	if (nrRecords==0) 
 	{
-		notifier->Error("I received 0 records from the database");
+		notifier->Error("[%s] -> I received 0 records from the database",ObjectName);
 		return false;
 	}
-	
-	IntervalStart = 0;
-	IntervalEnd   = RecordCount;
-	Initialized = TRUE;	
+	// aloca data :D
 
-	// fetch data
-	if (!database->FetchNextRow(VectPtr)) 
-	{		
-		notifier->Error("error fetching record %d from database", 0);
-		return false;
-	}		
-
-	// count the number of features that we have
-	FeatureCount = 0;
-	vectSize = VectPtr.GetCount();
-
-	for (UInt32 tr=0;tr<vectSize;tr++) 
+	if ((Records = new double [nrRecords*columns.nrFeatures])==NULL)
 	{
-		rec = VectPtr.GetPtrToObject(tr);
-		if (GML::Utils::GString::StartsWith(rec->Name,"Feat_",true))
-			FeatureCount++;
-	}
-	
-	// alloc memory for the cache
-	FeatureCache = new double [FeatureCount*RecordCount];
-	LabelStorage  = new double [RecordCount];
-	
-	if (!FeatureCache || !LabelStorage) 
-	{
-		notifier->Error("error allocating memory for the internal cache");
+		notifier->Error("[%s] -> Unable to allocate %ud bytes for data indexes !",ObjectName,nrRecords*columns.nrFeatures*sizeof(double));
 		return false;
 	}
-
-	FeaturesPtr = (double*)&FeatureCache[0*FeatureCount];	
-
-	for (UInt32 tr=0;tr<VectPtr.GetCount();tr++) 
+	if ((Labels = new double[nrRecords])==NULL)
 	{
-		DBRecord * dbrec = VectPtr.GetPtrToObject(tr);
+		notifier->Error("[%s] -> Unable to allocate %ud bytes for labels !",ObjectName,nrRecords*sizeof(double));
+		return false;
+	}
+	memset(Records,0,nrRecords*columns.nrFeatures*sizeof(double));
+	memset(Labels,0,nrRecords*sizeof(double));
+	// sunt exact la inceput
+	cPoz = Records;
 
-		// look for label
-		if (GML::Utils::GString::Equals(dbrec->Name, "Label", true)) 
+	for (tr=0;tr<nrRecords;tr++)
+	{
+		// cache
+		if ((tr % CachedRecords)==0)
 		{
-			// check if it's a signed type
-			if (dbrec->Type == BOOLVAL || dbrec->Type == UINT8VAL || dbrec->Type == UINT16VAL || dbrec->Type == UINT32VAL) {
-				notifier->Error("wrong data type for Label column");
-				database->FreeRow(VectPtr);
-				VectPtr.DeleteAll();
-				return false;
-			}
-
-			LabelStorage[0] = dbrec->DoubleVal;
-			LabelPos = tr;
-			continue;
-		}
-
-		/*
-		//look for the hash
-		if (GML::Utils::GString::Equals(dbrec->Name, "Hash", true)) 
-		{
-			// check if it's indeed a Hash data type
-			if (dbrec->Type != HASHVAL) {
-				notifier->Error("wrong data type for Hash column");
-				database->FreeRow(VectPtr);
-				VectPtr.DeleteAll();
-				return false;
-			}
-
-			record.Hash = dbrec->Hash;
-			HashPos = tr;
-			continue;
-		}
-		*/
-
-		//look for features
-		if (GML::Utils::GString::StartsWith(dbrec->Name, "Feat_", true)) 
-		{
-			// check that features are DOUBLEVAL
-			if (dbrec->Type != DOUBLEVAL && dbrec->Type != FLOATVAL) {
-				notifier->Error("wrong data type for Feat column");
-				database->FreeRow(VectPtr);
-				VectPtr.DeleteAll();
-				return false;
-			}
-
-			UInt32 nr;
-			if (GML::Utils::GString::ConvertToUInt32(&dbrec->Name[5],&nr)==false)
+			if (tr+CachedRecords<nrRecords)
+				tempStr.SetFormated("%s LIMIT %d,%d",Query.GetText(),tr,CachedRecords);
+			else
+				tempStr.SetFormated("%s LIMIT %d,%d",Query.GetText(),tr,nrRecords-tr);
+			//notifier->Info("%s",tempStr.GetText());
+			if (database->ExecuteQuery(tempStr.GetText())==false)
 			{
-				notifier->Error("Invalid number for Feat_xxx column: %s",dbrec->Name);
-				database->FreeRow(VectPtr);
-				VectPtr.DeleteAll();
+				notifier->Error("[%s] -> Unable to Execute query : %s !",ObjectName,tempStr.GetText());
 				return false;
 			}
-			FeaturesPtr[nr] = dbrec->DoubleVal;
 		}
-	}
-	
-	// free the data from database plugin
-	database->FreeRow(VectPtr);
-
-	// prepare the vector for our next round
-	VectPtr.DeleteAll();
-	
-	for (UInt32 i=1;i<RecordCount;i++) 
-	{		
-		// fetch data
-		if (!database->FetchNextRow(VectPtr)) 
+		if (database->FetchNextRow(VectPtr)==false)
 		{
-			notifier->Error("error fetching record %d from database", 0);
-			return false;
-		}	
-													
-		// put pointer from cache		
-		FeaturesPtr = (double*)&FeatureCache[i*FeatureCount];	
-		
-		LabelStorage[i] = VectPtr[LabelPos].DoubleVal;		
-						
-		// check to see if we have fields above the HashPos
-		if (VectPtr.GetCount()<=LabelPos+1) 
-		{
-			notifier->Error("error - the database vector has no features");
+			notifier->Error("[%s] -> Error reading #%d record !",ObjectName,tr);
 			return false;
 		}
-
-		// pass through the vector to store information		
-		for (UInt32 tr=LabelPos+1;tr<VectPtr.GetCount();tr++) 
+		// pentru fiecare record pun valorile
+		for (gr=0;gr<columns.nrFeatures;gr++)
 		{
-			DBRecord * dbrec = VectPtr.GetPtrToObject(tr);
-
-			// check that features are DOUBLEVAL
-			if (dbrec->Type != DOUBLEVAL && dbrec->Type != FLOATVAL) 
-			{
-				notifier->Error("wrong data type for Feat column");
-				database->FreeRow(VectPtr);
-				VectPtr.DeleteAll();
+			if (UpdateDoubleValue(VectPtr,columns.indexFeature[gr],cValue)==false)
 				return false;
-			}
-
-			UInt32 nr;
-			if (GML::Utils::GString::ConvertToUInt32(&dbrec->Name[5],&nr)==false)
-			{
-				notifier->Error("Invalid number for Feat_xxx column: %s",dbrec->Name);
-				database->FreeRow(VectPtr);
-				VectPtr.DeleteAll();
-				return false;
-			}
-			FeaturesPtr[nr] = dbrec->DoubleVal;
-		}				
-				
-		// free the data from database plugin
-		database->FreeRow(VectPtr);
-
-		// prepare the vector for our next round
-		VectPtr.DeleteAll();
+			if (cValue==1.0)
+				cPoz[gr] = 1.0;
+			else
+				cPoz[gr] = -1.0;
+		}
+		// pun si label-ul
+		if (UpdateDoubleValue(VectPtr,columns.indexLabel,cValue)==false)
+			return false;
+		if (cValue==1.0)
+			Labels[tr] = 1.0;
+		else
+			Labels[tr] = -1.0;
+		// trecem la urmatorul record
+		cPoz+=columns.nrFeatures;
 	}	
-
-	notifier->Info("FullCacheConnector data (Records=%d,Features=%d)",RecordCount,FeatureCount);
-
+	// all ok , am incarcat datele
+	FeatureCount = columns.nrFeatures;
+	notifier->Info("[%s] -> Records=%d,Features=%d,MemSize=%d",ObjectName,nrRecords,columns.nrFeatures,(nrRecords+1)*sizeof(double)*columns.nrFeatures);
 	return true;
 }
 
@@ -277,41 +137,48 @@ bool FullCacheConnector::FreeMLRecord( MLRecord &record )
 
 bool FullCacheConnector::Close()
 {
-	return database->Disconnect();	
+	if (database)
+		return database->Disconnect();	
+	return true;
 }
 
 UInt32 FullCacheConnector::GetTotalRecordCount()
 {
-	if (!Initialized) return 0;
-	return RecordCount;
+	return nrRecords;
 }
 
 FullCacheConnector::FullCacheConnector()
 {
-	 RecordCount = 0;
+	 nrRecords = 0;
 	 FeatureCount = 0;
-	 Initialized = FALSE;
 
-	 FeatureCache = NULL;
-	 LabelStorage  = NULL;	 
+	 Records = NULL;
+	 Labels  = NULL;	 
 
 	 ObjectName = "FullCacheConnector";
 }
 
 FullCacheConnector::~FullCacheConnector()
 {
-	if (FeatureCache != NULL) 	
-		delete FeatureCache;
+	if (Records != NULL) 	
+		delete Records;
 
-	if (LabelStorage != NULL)
-		delete LabelStorage;
+	if (Labels!= NULL)
+		delete Labels;
+
+	Records = NULL;
+	Labels = NULL;
 }
 
 bool FullCacheConnector::GetRecordLabel( double &label,UInt32 index )
 {
-	if (index < IntervalStart || index >= IntervalEnd)
+	if (index >= nrRecords) 
+	{
+		notifier->Error("[%s] -> index is out of range, it has to be less than %d", nrRecords);
 		return false;
+	}
 
-	label = LabelStorage[index];
+
+	label = Labels[index];
 	return true;
 }
