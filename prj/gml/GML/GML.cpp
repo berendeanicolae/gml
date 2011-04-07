29c0e528962c9efe509ef45ad6e5a4f6d87cfb00
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-07 12:28:02 +0000

    GML with a lot of options

--- prj/gml/GML/GML.cpp
+++ prj/gml/GML/GML.cpp
@@ -8,7 +8,25 @@ int  Error(char *message)
 	printf("[ERROR] : %s\n",message);
 	return -1;
 }
-int Run(char *templateName)
+bool FindGMLLibPath(GML::Utils::GString &gmlLib)
+{
+	HMODULE					hModule;
+	GML::Utils::GString		tmp;
+	char					temp[2048];
+
+	
+	if ((hModule = GetModuleHandleA("gmllib.dll"))==NULL)
+		return false;
+	if (GetModuleFileNameA(hModule,temp,2048)==0)
+		return false;
+	if (tmp.Set(temp)==false)
+		return false;
+	if (tmp.CopyPathName(&gmlLib)==false)
+		return false;
+
+	return true;
+}
+int  Run(char *templateName)
 {
 	GML::Utils::AttributeList	config;
 	GML::Utils::GString			algName;
@@ -49,7 +67,103 @@ int Run(char *templateName)
 	printf("Algorithm '%s' done !\n",algName.GetText());
 	return 0;
 }
-void ShowHelp()
+int  Info(char *objectName)
+{
+	GML::Utils::AttributeList	attr;
+	GML::Utils::Attribute		*a;
+	GML::Utils::GString			tmp,list;
+
+	if (GML::Builder::GetPluginProperties(objectName,attr)==false)
+	{
+		printf("[ERROR] There is no algorithm, notifier, database or conector with the name : %s\n",objectName);
+		return 1;
+	}
+	printf("[%s]\n---------------------------------------------------------------------------------\n",objectName);
+	for (UInt32 tr=0;tr<attr.GetCount();tr++)
+	{
+		if ((a=attr.Get(tr))==NULL)
+			continue;
+		tmp.Set(a->Name);
+		while (tmp.Len()<20)
+			tmp.AddChar(' ');
+		tmp.Add(" = ");
+		switch (a->AttributeType)
+		{
+			case GML::Utils::AttributeList::BOOLEAN:
+				if (*(bool *)a->Data)
+					tmp.Add("True");
+				else
+					tmp.Add("False");
+				break;
+			case GML::Utils::AttributeList::UINT8:
+				tmp.AddFormated("%u",*(UInt8 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::UINT16:
+				tmp.AddFormated("%u",*(UInt16 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::UINT32:
+				tmp.AddFormated("%u",*(UInt32 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::INT8:
+				tmp.AddFormated("%d",*(Int8 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::INT16:
+				tmp.AddFormated("%d",*(Int16 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::INT32:
+				tmp.AddFormated("%d",*(Int32 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::FLOAT:
+				tmp.AddFormated("%.3f",*(float *)a->Data);
+				break;
+			case GML::Utils::AttributeList::DOUBLE:
+				tmp.AddFormated("%.3lf",*(double *)a->Data);
+				break;
+			case GML::Utils::AttributeList::STRING:
+				tmp.AddFormated("'%s'",(char *)a->Data);
+				if ((a->GetFlags() & GML::Utils::FL_LIST)!=0)
+				{
+					if (a->GetListItem(list))
+						tmp.AddFormated(" one of [%s]",list.GetText());
+				}
+				break;
+			default:
+				tmp.AddFormated("<?Type=%d>",a->AttributeType);
+				break;
+		}
+		printf("  %s\n",tmp.GetText());
+	}
+	printf("---------------------------------------------------------------------------------\n");
+	return 0;
+}
+int  ShowObjects(char *folder,char *ext)
+{
+	HANDLE					hFind;
+	GML::Utils::GString		gmlLib,tmp;
+	WIN32_FIND_DATA			dt;
+
+	if (FindGMLLibPath(gmlLib)==false)
+		return Error("Unable to locate gmllib.dll");
+	
+	tmp.SetFormated("*%s",ext);
+	gmlLib.PathJoinName(folder);
+	gmlLib.PathJoinName(tmp.GetText());
+	//printf("%s\n",gmlLib.GetText());
+	if ((hFind = FindFirstFile(gmlLib.GetText(),&dt))==INVALID_HANDLE_VALUE)
+		return Error("FindFirstFile failed !");
+	do
+	{
+		if (GML::Utils::GString::EndsWith(dt.cFileName,ext,true))
+		{
+			tmp.Set(dt.cFileName);
+			tmp.Replace(ext,"");
+			printf("  %s\n",tmp.GetText());
+		}
+	} while (FindNextFile(hFind,&dt));
+	FindClose(&dt);
+	return 0;
+}
+int  ShowHelp()
 {
 	printf("GML.EXE , Copyright @2011 , Gavrilut Dragos & co\n");
 	printf("Build on %s (%s)\n",__DATE__,__TIME__);
@@ -57,22 +171,45 @@ void ShowHelp()
 	printf("Where: command is one of the following\n");
 	printf("       run <template_file>  -> executes a template file\n");
 	printf("       info <object_name>   -> shows informations about a specific Algorithm,Conector,DataBase or Notifier\n");
+	printf("       algorithms           -> shows a list of existing algorithms\n");
+	printf("       connectors           -> shows a list of existing connectors\n");
+	printf("       notifiers            -> shows a list of existing notifiers\n");
+	printf("       databases            -> shows a list of existing databases\n");
 
 	printf("\n");
+	return 0;
 }
 int  main(int argc, char* argv[])
 {
 	if (argc<=1)
-	{
-		ShowHelp();
-		return 1;
-	}
+		return ShowHelp();
+	// run
 	if (GML::Utils::GString::Equals(argv[1],"run",true))
 	{
 		if (argc!=3)
 			return Error("run command requare a parameter (a template file)");
 		return Run(argv[2]);
 	}
+	// info
+	if (GML::Utils::GString::Equals(argv[1],"info",true))
+	{
+		if (argc!=3)
+			return Error("info command requare a parameter (a name for a an Algorithm, Conector, DataBase or Notifier)");
+		return Info(argv[2]);
+	}
+	// algorithms
+	if (GML::Utils::GString::Equals(argv[1],"algorithms",true))
+		return ShowObjects(ALGORITHM_FOLDER,ALGORITHM_EXT);
+	// connectors
+	if (GML::Utils::GString::Equals(argv[1],"connectors",true))
+		return ShowObjects(CONNECTOR_FOLDER,CONNECTOR_EXT);
+	// notifiers
+	if (GML::Utils::GString::Equals(argv[1],"notifiers",true))
+		return ShowObjects(NOTIFYER_FOLDER,NOTIFYER_EXT);
+	// databases
+	if (GML::Utils::GString::Equals(argv[1],"databases",true))
+		return ShowObjects(DATABASE_FOLDER,DATABASE_EXT);
+
 	printf("[ERROR] Unknwon command : %s \n",argv[1]);
 	return -1;
 }
