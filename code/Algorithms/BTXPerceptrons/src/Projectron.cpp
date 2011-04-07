55c6a603b0c3f88141b4377fdab040572760df99
alexandru.bt <Alexandru.bt@gmail.com>
2011-04-07 10:47:53 +0000

    Update

--- code/Algorithms/BTXPerceptrons/src/Projectron.cpp
+++ code/Algorithms/BTXPerceptrons/src/Projectron.cpp
@@ -1,73 +1,134 @@
 #include "Projectron.h"
 
+UInt32 CountActiveFeatures(double *feat,UInt32 nrElements)
+	{
+		UInt32	sum = 0;
+		while (nrElements>0)
+		{
+			if ((*feat)!=0)
+				sum++;
+			feat++;
+			nrElements--;
+		}
+		return sum;
+	}
+
 Projectron::Projectron()
 {
 	ObjectName = "Projectron";
-
-	db = NULL;
-	con = NULL;
-
-	LinkPropertyToString("Connector",strConector,"","Conectorul la care sa se conecteze");
-	LinkPropertyToString("Notifier",strNotificator,"","Notificatorul la care sa se conecteze");
-	LinkPropertyToString("DataBase",strDB,"","Baza de date la care sa se conecteze");
 	LinkPropertyToUInt32("KernelFunction",kernelFunction,POLYNOMIAL_KER,"!!LIST:Polinomiala=0,Sigmoida!!\nMetrica folosita");
-
-	SetPropertyMetaData("Command","!!LIST:None=0,Train,Test!!");
 }
 
-bool Projectron::Init()
-{
-	if ((notif = GML::Builder::CreateNotifier(strNotificator.GetText()))==NULL)
-		return false;
-	if ((db = GML::Builder::CreateDataBase(strDB.GetText(),*notif))==NULL)
-	{
-		notif->Error("[%s] -> Unable to create Database (%s)",strDB.GetText());
-		return false;
-	}
-	if (db->Connect()==false)
-	{
-		notif->Error("[%s] -> Unable to connesct to Database (%s)",ObjectName,strDB.GetText());
-		return false;
-	}
-	if ((con = GML::Builder::CreateConnectors(strConector.GetText(),*notif,*db))==NULL)
-	{
-		notif->Error("[%s] -> Unable to create Conector (%s)",ObjectName,strConector.GetText());
-		return false;
-	}
-
-	notif->Info("Init: %d");
+bool Projectron::OnInit(){
+	Space.Create(con->GetRecordCount());
+	K.Create(0,0);
 	return true;
 }
 
-bool Projectron::PerformTrain()
-{
-	return true;
-}
-bool Projectron::PerformTest()
-{
-	return true;
-}
+bool Projectron::Train(PerceptronThreadData *ptd,GML::Utils::Indexes *indexes,bool clearDelta,bool addDeltaToPrimary){
+	UInt32	*ptrIndex = indexes->GetList();
+	UInt32	count,act_featCount;
+	UInt32	nrFeatures = con->GetFeatureCount();
+	double	*w = ptd->Primary.Weight;
+	double	*b = ptd->Primary.Bias;
+	double	error;
+	double	sum;
 
-void Projectron::OnExecute()
-{
-	switch (Command)
+	ptd->corectelyClasify = 0;
+	if (ptd->Range.End>indexes->Len())
 	{
+		notif->Error("[%s] -> (TRAIN)::Invalid Range (%d..%d) for thread %d with %d records",ObjectName,ptd->Range.Start,ptd->Range.End,ptd->ID,indexes->Len());
+		return false;
+	}
+	ptrIndex += ptd->Range.Start;
+	count=ptd->Range.Size();
 
-	case COMMAND_TRAIN:
-		if(!PerformTrain()){
-			notif->Error("Error encountered on training!");
-			return;
+	if (ptd->Delta.Weight!=NULL)
+	{
+		w = ptd->Delta.Weight;
+		b = ptd->Delta.Bias;
+		if (clearDelta)
+		{
+			memset(w,0,sizeof(double)*ptd->Delta.Count);
+			(*b)=0;
 		}
-		notif->Info("Training sequence performed succesfully.");
-		return;
-	case COMMAND_TEST:
-		if(!PerformTest()){
-			notif->Error("Error encountered on testing!!");
-		}
-		notif->Info("Testing sequence performed succesfully.");
-		return;
-	case COMMAND_NONE:
-			notif->Info("[%s] -> Nothing to do ... ",ObjectName);
-			return;
 	}
+	if (!useB)
+		(*ptd->Primary.Bias)=0;
+
+	while (count>0)
+	{		
+		if (con->GetRecord(ptd->Record,*ptrIndex)==false)
+		{
+			notif->Error("(TRAIN)::Error reading record #%d from thread #%d",(*ptrIndex),ptd->ID);
+			return false;
+		}
+		sum = GML::ML::VectorOp::ComputeVectorsSum(ptd->Record.Features,ptd->Primary.Weight,nrFeatures)+(*ptd->Primary.Bias);
+
+		if ((sum * ptd->Record.Label)<=0)
+		{
+			switch (adjustWeightMode)
+			{
+				case ADJUST_WEIGHT_LEARNING_RATE:
+					error = ptd->Record.Label * learningRate;
+					break;
+				case ADJUST_WEIGHT_USE_WEIGHT:
+					error = ptd->Record.Label * ptd->Record.Weight * learningRate;
+					break;
+				case ADJUST_WEIGHT_LEASTMEANSQUARE:					
+					if (sum==0)
+						error = ptd->Record.Label * learningRate;
+					else
+						error = -learningRate * sum;
+					break;
+				case ADJUST_WEIGHT_SPLIT_LEARNING_RATE:
+					act_featCount = CountActiveFeatures(ptd->Record.Features,nrFeatures)+1; // +1 pentru Bias si ca sa fiu sigur ca e mai mare ca 0
+					error = (ptd->Record.Label * learningRate) / ((double)act_featCount);
+					break;
+				case ADJUST_WEIGHT_SPLIT_LEASTMEANSQUARE:
+					if (sum==0)
+						error = ptd->Record.Label * learningRate;
+					else
+						error = -learningRate * sum;
+					act_featCount = CountActiveFeatures(ptd->Record.Features,nrFeatures)+1; // +1 pentru Bias si ca sa fiu sigur ca e mai mare ca 0
+					error = error / ((double)act_featCount);
+					break;
+				case ADJUST_WEIGHT_USE_FEAT_WEIGHT:
+					error = ptd->Record.Label * learningRate;
+					break;
+				default:
+					error = 0;
+					break;
+			};
+			
+			if (adjustWeightMode==ADJUST_WEIGHT_USE_FEAT_WEIGHT)
+			{
+				GML::ML::VectorOp::AdjustPerceptronWeights(ptd->Record.Features,w,nrFeatures,error,featWeight);
+				if (useB)
+					(*b) += error;
+			} else {
+				GML::ML::VectorOp::AdjustPerceptronWeights(ptd->Record.Features,w,nrFeatures,error);
+				if (useB)
+					(*b) += error;
+			}
+		} else {
+			ptd->corectelyClasify ++;
+		}
+		count--;
+		ptrIndex++;
+	}
+	if ((ptd->Delta.Weight!=NULL) && (addDeltaToPrimary))
+		ptd->Primary.Add(ptd->Delta);
+	return true;
 }
+
+bool Projectron::PerformTrainIteration()
+{
+	return GenericPerceptron::Train(&FullData,false,false);
+}
+
+bool Projectron::PerformTestIteration()
+{
+	return Test(&FullData);
+}
+
