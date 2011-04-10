de0d3a4d026324f48eab544bcd66c6e7b9db2423
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-10 20:05:36 +0000

    GetRecordHash for MapConnector & SplitConnector

--- trunk/code/Connectors/SplitConnector/inc/SplitConnector.h
+++ trunk/code/Connectors/SplitConnector/inc/SplitConnector.h
@@ -44,6 +44,7 @@ public:
 	bool					GetRecordLabel( double &label,UInt32 index );
 	UInt32					GetFeatureCount();	
 	bool					GetRecord( MLRecord &record,UInt32 index,UInt32 recordMask=0 );
+	bool					GetRecordHash(GML::DB::RecordHash &recHash,UInt32 index);
 	bool					CreateMlRecord( MLRecord &record );
 	bool					SetRecordInterval( UInt32 start, UInt32 end );
 	bool					FreeMLRecord( MLRecord &record );
