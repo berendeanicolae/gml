fb1249532fce1d6056377e2af985d6c291d72157
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-11 10:33:45 +0000

    Add new stuff

---
+++ trunk/code/Algorithms/GDTDistances/inc/GenericDistAlgorithm.h
@@ -0,0 +1,15 @@
+#ifndef __GEN_DIST_ALG__
+#define __GEN_DIST_ALG__
+
+#include "gmllib.h"
+
+class GenericDistAlgorithm : public GML::Algorithm::IAlgorithm
+{
+public:
+	GenericDistAlgorithm();
+	
+};
+
+
+#endif
+
---
+++ trunk/code/Algorithms/GDTDistances/src/GenericDistAlgorithm.cpp
@@ -0,0 +1,6 @@
+#include "GenericDistAlgorithm.h"
+
+GenericDistAlgorithm::GenericDistAlgorithm()
+{
+	ObjectName = "GenericDistance";
+}
