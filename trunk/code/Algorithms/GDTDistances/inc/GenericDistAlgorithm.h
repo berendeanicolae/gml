62fc1f6f81db0cfcf4078ea84e1f8c98bd57a866
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-12 00:16:22 +0000

    GenericDistAlg -> functions / pararelizare

--- trunk/code/Algorithms/GDTDistances/inc/GenericDistAlgorithm.h
+++ trunk/code/Algorithms/GDTDistances/inc/GenericDistAlgorithm.h
@@ -6,13 +6,24 @@
 struct ThreadData
 {
 	UInt32				ID;
-	GML::ML::MLRecord	Rec;
+	GML::ML::MLRecord	Rec1,Rec2;
 
 };
+
+#define SAVE_CACHE_SIZE		1024
 
 class GenericDistAlgorithm : public GML::Algorithm::IAlgorithm
 {
 protected:
+	enum {
+		COMMAND_NONE = 0,
+		COMMAND_COMPUTE
+	};
+	enum {
+		SAVE_HASHES_AS_TEXT = 0,
+		SAVE_HASHES_AS_BINARY
+	};
+
 	// proprietati
 	UInt32							threadsCount;
 	GML::DB::IDataBase				*db;
@@ -22,20 +33,29 @@ protected:
 	GML::Utils::GString				Conector;
 	GML::Utils::GString				DataBase;
 	GML::Utils::GString				Notifier;
+	GML::Utils::GString				HashFileName;
+	UInt32							HashStoreMethod;
 
 	GML::Utils::ThreadParalelUnit	*tpu;
+	GML::Utils::Indexes				indexesPozitive,indexesNegative;
+	GML::Utils::GTFVector<UInt8>	RecordsStatus;
 public:
 	ThreadData						*ptData;
 
-
+	
 public:
 	GenericDistAlgorithm();
 
+
 	virtual void					OnRunThreadCommand(ThreadData &td,UInt32 command) {};
 	virtual bool					OnInit();
+	virtual bool					OnCompute();
+
+	bool							SaveHashResult();
+	bool							ExecuteParalelCommand(UInt32 command);
 	bool							Init();
 	void							OnExecute();
-	
+	bool							CreatePozitiveAndNegativeIndexes();
 };
 
 
--- trunk/code/Algorithms/GDTDistances/src/GenericDistAlgorithm.cpp
+++ trunk/code/Algorithms/GDTDistances/src/GenericDistAlgorithm.cpp
@@ -13,12 +13,69 @@ GenericDistAlgorithm::GenericDistAlgorithm()
 	db = NULL;
 	con = NULL;
 
-	SetPropertyMetaData("Command","!!LIST:None=0,Train,Test!!");
+	SetPropertyMetaData("Command","!!LIST:None=0,Compute!!");
 
 	LinkPropertyToString("DataBase"					,DataBase				,"");
 	LinkPropertyToString("Connector"				,Conector				,"");
 	LinkPropertyToString("Notifier"					,Notifier				,"");
+	LinkPropertyToString("HashFileName"				,HashFileName			,"Name of the file with the record hash list result.");
+	LinkPropertyToUInt32("HashStoreMethod"			,HashStoreMethod		,0,"!!LIST:SaveAsTextFile=0,SaveAsBinaryFile!!");
 
+}
+bool	GenericDistAlgorithm::CreatePozitiveAndNegativeIndexes()
+{
+	UInt32	tr;
+	UInt32	pozitiveCount,negativeCount,nrRecords;
+	double	label;
+	bool	res;
+
+	notif->Info("[%s] -> Creating Pozitive and Negative indexes ... ",ObjectName);
+	pozitiveCount = negativeCount = 0;
+	nrRecords = con->GetRecordCount();
+	
+	for (tr=0;tr<nrRecords;tr++)
+	{
+		if (con->GetRecordLabel(label,tr)==false)
+		{
+			notif->Error("[%s] -> Unable to read label for record #%d",ObjectName,tr);
+			return false;
+		}
+		if (label==1.0)
+			pozitiveCount++;
+		else
+			negativeCount++;
+	}
+	notif->Info("[%s] -> Pozitive = %d ,Negative = %d",ObjectName,pozitiveCount,negativeCount);
+	if (indexesPozitive.Create(pozitiveCount)==false)
+	{
+		notif->Error("[%s] -> Unable to alloc %d pozitive indexes",ObjectName,pozitiveCount);
+		return false;
+	}
+	if (indexesNegative.Create(negativeCount)==false)
+	{
+		notif->Error("[%s] -> Unable to alloc %d negative indexes",ObjectName,negativeCount);
+		return false;
+	}
+	// adaugam si indexii
+	for (tr=0;tr<nrRecords;tr++)
+	{
+		if (con->GetRecordLabel(label,tr)==false)
+		{
+			notif->Error("[%s] -> Unable to read label for record #%d",ObjectName,tr);
+			return false;
+		}
+		if (label==1.0)
+			res = indexesPozitive.Push(tr);
+		else
+			res = indexesNegative.Push(tr);
+		if (res==false)
+		{
+			notif->Error("[%s] -> Unable to add index #%d",ObjectName,tr);
+			return false;
+		}		
+	}
+	notif->Info("[%s] -> Pozitive and Negative indexes created !",ObjectName);
+	return true;
 }
 bool	GenericDistAlgorithm::Init()
 {
@@ -54,6 +111,20 @@ bool	GenericDistAlgorithm::Init()
 	if (threadsCount<1)
 		threadsCount = 1;
 	
+	if (RecordsStatus.Create(con->GetRecordCount())==false)
+	{
+		notif->Error("[%s] -> Unable to create RecordsStatus vector (for %d records)",ObjectName,con->GetRecordCount());
+		return false;
+	}
+	// resetare
+	if (RecordsStatus.Resize(con->GetRecordCount())==false)
+	{
+		notif->Error("[%s] -> Unable to alloc RecordsStatus vector (for %d records)",ObjectName,con->GetRecordCount());
+		return false;
+	}
+	// setare totul pe 0
+	memset(RecordsStatus.GetVector(),0,sizeof(UInt8) * RecordsStatus.Len());
+
 	if ((ptData = new ThreadData[threadsCount])==NULL)
 	{
 		notif->Error("[%s] -> Unable to allocate ThreadData[%d]",ObjectName,threadsCount);
@@ -62,9 +133,14 @@ bool	GenericDistAlgorithm::Init()
 	for (tr=0;tr<threadsCount;tr++)
 	{
 		ptData[tr].ID = tr;
-		if (con->CreateMlRecord(ptData[tr].Rec)==false)
+		if (con->CreateMlRecord(ptData[tr].Rec1)==false)
 		{
-			notif->Error("[%s] -> Unable to create ThreadData[%d].Rec",ObjectName,tr);
+			notif->Error("[%s] -> Unable to create ThreadData[%d].Rec1",ObjectName,tr);
+			return false;
+		}
+		if (con->CreateMlRecord(ptData[tr].Rec2)==false)
+		{
+			notif->Error("[%s] -> Unable to create ThreadData[%d].Rec2",ObjectName,tr);
 			return false;
 		}
 	}
@@ -84,4 +160,146 @@ bool	GenericDistAlgorithm::Init()
 	}
 
 	return OnInit();
+}
+bool    GenericDistAlgorithm::ExecuteParalelCommand(UInt32 command)
+{
+	UInt32	tr;
+
+	// executie
+	for (tr=0;tr<threadsCount;tr++)
+		if (tpu[tr].Execute(command)==false)
+		{
+			notif->Error("[%s] -> Error on runnig thread #%d",ObjectName,tr);
+			return false;
+		}
+	// asteptare
+	for (tr=0;tr<threadsCount;tr++)
+		if (tpu[tr].WaitToFinish()==false)
+		{
+			notif->Error("[%s] -> WaitToFinish failed on thread #%d",ObjectName,tr);
+			return false;
+		}
+	// all ok
+	return true;
+}
+bool	GenericDistAlgorithm::OnInit()
+{
+	return true;
+}
+void	GenericDistAlgorithm::OnExecute()
+{
+	StopAlgorithm = false;
+	
+	switch (Command)
+	{
+		case COMMAND_NONE:
+			notif->Info("[%s] -> Nothing to do ... ",ObjectName);
+			return;
+		case COMMAND_COMPUTE:
+			OnCompute();
+			return;
+	};
+	notif->Error("[%s] -> Unkwnown command ID : %d",ObjectName,Command);
+}
+bool	GenericDistAlgorithm::SaveHashResult()
+{
+	GML::Utils::File		f;
+	GML::Utils::Vector		Cache;
+	UInt32					recSize,tr,count;
+	UInt8					*status,*p;
+	GML::DB::RecordHash		rHash;
+	GML::Utils::GString		temp;
+
+	switch (HashStoreMethod)
+	{
+		case SAVE_HASHES_AS_TEXT:
+			recSize = 32+2;
+			break;
+		case SAVE_HASHES_AS_BINARY:
+			recSize = 16;
+			break;
+		default:
+			notif->Error("[%s] -> Unknown HashStoreMethod = %d",ObjectName,HashStoreMethod);
+			return false;
+	};
+	status = RecordsStatus.GetVector();
+	for (tr=0,count=0;tr<RecordsStatus.Len();tr++,status++)
+	{
+		if ((*status)!=0)
+			count++;
+	}
+	if (HashFileName.Len()==0)
+	{
+		notif->Error("[%s] -> You need to set 'HashFileName' property with the name of the file where you want to save your results",ObjectName);
+		return false;
+	}
+	if (f.Create(HashFileName.GetText())==false)
+	{
+		notif->Error("[%s] -> Unable to create %s",ObjectName,HashFileName.GetText());
+		return false;
+	}
+	status = RecordsStatus.GetVector();
+	while (true)
+	{
+		if (Cache.Create(SAVE_CACHE_SIZE,recSize)==false)
+		{
+			notif->Error("[%s] -> Unable to allocate %d bytes for caching ...",ObjectName,recSize * SAVE_CACHE_SIZE);
+			break;
+		}
+		// pentru binary method , scriu cateva date
+		if (HashStoreMethod==SAVE_HASHES_AS_BINARY)
+		{
+			if (f.Write("HASHLIST",8)==false)
+				break;
+			if (f.Write(&count,sizeof(UInt32))==false)
+				break;
+		}
+		for (tr=0;tr<RecordsStatus.Len();tr++,status++)
+		{
+			if ((*status)!=0)
+			{
+				// scriu datele
+				if (con->GetRecordHash(rHash,tr)==false)
+				{
+					notif->Error("[%s] -> Unable to read hash for record #%d",ObjectName,tr);
+					break;
+				}
+				switch (HashStoreMethod)
+				{
+					case SAVE_HASHES_AS_TEXT:
+						rHash.ToString(temp);
+						p = (UInt8 *)temp.GetText();
+						break;
+					case SAVE_HASHES_AS_BINARY:
+						recSize = 16;
+						p = (UInt8 *)&rHash.Hash.bValue[0];
+						break;
+				};
+				if (Cache.GetSize()>=SAVE_CACHE_SIZE)
+				{
+					if (f.Write(Cache.GetVector(),Cache.GetSize()*recSize)==false)
+						break;
+					if (Cache.Resize(0)==false)
+						break;
+				}
+				if (Cache.Push(p)==false)
+					break;
+			}
+		}
+		// daca nu am ajuns la final , am o eroare
+		if (tr!=RecordsStatus.Len())
+			break;
+		if (Cache.GetSize()>=0)
+		{
+			if (f.Write(Cache.GetVector(),Cache.GetSize()*recSize)==false)
+				break;
+		}
+		f.Close();
+		notif->Info("[%s] Hashes saved ok in %s (%d hashesh)",ObjectName,HashFileName.GetText(),count);
+		return true;
+	}
+	f.Close();
+	DeleteFileA(HashFileName.GetText());
+	notif->Error("[%s] -> Unable to write hashes to %s",ObjectName,HashFileName.GetText());
+	return false;
 }
