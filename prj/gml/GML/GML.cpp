09953b0dad30e40154ab6d3774b6cd354ee378f3
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-12 21:41:23 +0000

    gml wiith template options

--- prj/gml/GML/GML.cpp
+++ prj/gml/GML/GML.cpp
@@ -67,6 +67,98 @@ int  Run(char *templateName)
 	printf("Algorithm '%s' done !\n",algName.GetText());
 	return 0;
 }
+int  Template(char *objectName,char *templateName)
+{
+	GML::Utils::AttributeList	attr;
+	GML::Utils::Attribute		*a;
+	GML::Utils::GString			tmp,result,desc;
+	GML::Utils::File			f;
+	
+	if (GML::Builder::GetPluginProperties(objectName,attr)==false)
+	{
+		printf("[ERROR] There is no algorithm, notifier, database or conector with the name : %s\n",objectName);
+		return 1;
+	}
+	result.SetFormated("AlgorithmName        = %s\n;----------------------------------------------------------------------------\n\n",objectName);
+	for (UInt32 tr=0;tr<attr.GetCount();tr++)
+	{
+		if ((a=attr.Get(tr))==NULL)
+			continue;
+		tmp.Set(a->Name);
+		while (tmp.Len()<20)
+			tmp.AddChar(' ');
+		tmp.Add(" = ");
+		switch (a->AttributeType)
+		{
+			case GML::Utils::AttributeList::BOOLEAN:
+				if (*(bool *)a->Data)
+					tmp.Add("True");
+				else
+					tmp.Add("False");
+				break;
+			case GML::Utils::AttributeList::UINT8:
+				tmp.AddFormated("%u",*(UInt8 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::UINT16:
+				tmp.AddFormated("%u",*(UInt16 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::UINT32:
+				tmp.AddFormated("%u",*(UInt32 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::INT8:
+				tmp.AddFormated("%d",*(Int8 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::INT16:
+				tmp.AddFormated("%d",*(Int16 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::INT32:
+				tmp.AddFormated("%d",*(Int32 *)a->Data);
+				break;
+			case GML::Utils::AttributeList::FLOAT:
+				tmp.AddFormated("%.3f",*(float *)a->Data);
+				break;
+			case GML::Utils::AttributeList::DOUBLE:
+				tmp.AddFormated("%.3lf",*(double *)a->Data);
+				break;
+			case GML::Utils::AttributeList::STRING:
+				tmp.AddFormated("\"%s\"",(char *)a->Data);
+				break;
+			default:
+				tmp.AddFormated("<?Type=%d>",a->AttributeType);
+				break;
+		};
+		desc.Set("");
+		if (a->GetDescription(desc))
+		{
+			desc.Replace("\n",".");
+			desc.Replace("\r",".");
+			desc.Strip();
+			if (desc.Len()>0)
+				result.AddFormated(";%s\n",desc.GetText());
+		}
+		desc.Set("");
+		if (((a->GetFlags() & GML::Utils::FL_LIST)!=0) && (a->AttributeType==GML::Utils::AttributeList::STRING))
+		{
+			if (a->GetListItem(desc))
+				result.AddFormated(";one of [%s]\n",desc.GetText());
+		}
+		result.AddFormated("%s\n\n",tmp.GetText());		
+	}
+	if (f.Create(templateName)==false)
+	{
+		printf("[ERROR] Unable to create : %s\n",templateName);
+		return 1;
+	}
+	if (f.Write(result.GetText(),result.Len())==false)
+	{
+		printf("[ERROR] Unable to write to file : %s\n",templateName);
+		f.Close();
+		DeleteFileA(templateName);
+		return 1;
+	}
+	f.Close();
+	return 0;
+}
 int  Info(char *objectName)
 {
 	GML::Utils::AttributeList	attr;
@@ -169,12 +261,13 @@ int  ShowHelp()
 	printf("Build on %s (%s)\n",__DATE__,__TIME__);
 	printf("Usage: gml.exe command <parameters>\n");
 	printf("Where: command is one of the following\n");
-	printf("       run <template_file>  -> executes a template file\n");
-	printf("       info <object_name>   -> shows informations about a specific Algorithm,Conector,DataBase or Notifier\n");
-	printf("       algorithms           -> shows a list of existing algorithms\n");
-	printf("       connectors           -> shows a list of existing connectors\n");
-	printf("       notifiers            -> shows a list of existing notifiers\n");
-	printf("       databases            -> shows a list of existing databases\n");
+	printf("       run <template_file>       -> executes a template file\n");
+	printf("       info <object_name>        -> shows informations about a specific Algorithm,Conector,DataBase or Notifier\n");
+	printf("       template <object> <file>  -> creates a template for a specifiy object\n");
+	printf("       algorithms                -> shows a list of existing algorithms\n");
+	printf("       connectors                -> shows a list of existing connectors\n");
+	printf("       notifiers                 -> shows a list of existing notifiers\n");
+	printf("       databases                 -> shows a list of existing databases\n");
 
 	printf("\n");
 	return 0;
@@ -196,6 +289,13 @@ int  main(int argc, char* argv[])
 		if (argc!=3)
 			return Error("info command requare a parameter (a name for a an Algorithm, Conector, DataBase or Notifier)");
 		return Info(argv[2]);
+	}
+	// template
+	if (GML::Utils::GString::Equals(argv[1],"template",true))
+	{
+		if (argc!=4)
+			return Error("template command requare two parameters (a name for a an Algorithm, Conector, DataBase or Notifier and a file name)");
+		return Template(argv[2],argv[3]);
 	}
 	// algorithms
 	if (GML::Utils::GString::Equals(argv[1],"algorithms",true))
