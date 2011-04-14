d7bb19197e908a424118e7aea81d31eecf9efec8
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-14 17:38:28 +0000

    LinearVote fixup

--- code/Algorithms/LinearVote/inc/LinearVote.h
+++ code/Algorithms/LinearVote/inc/LinearVote.h
@@ -1,19 +1,40 @@
-#ifndef __CACHE_BUILDER__
-#define __CACHE_BUILDER__
+#ifndef __LINEAR_VOTE__
+#define __LINEAR_VOTE__
 
 #include "gmllib.h"
 
-class CacheBuilder: public GML::Algorithm::IAlgorithm
+class PerceptronVector
 {
-	GML::Utils::GString		CacheName;
+public:
+	double				Bias;
+	double				*Weight;
+	UInt32				Count;
+	double				Vote;
+	GML::Utils::GString	FileName;
+public:
+	PerceptronVector();
+	~PerceptronVector();
+
+	void	Destroy();
+	bool	Create(UInt32 count);
+};
+
+class LinearVote: public GML::Algorithm::IAlgorithm
+{	
 	GML::Utils::GString		Conector;
 	GML::Utils::GString		DataBase;
 	GML::Utils::GString		Notifier;
+	GML::Utils::GString		WeightFiles;
 	GML::DB::IDataBase		*db;
 	GML::ML::IConnector		*con;
 
+	PerceptronVector		*pVectors;
+
+
+	bool					Create(PerceptronVector &pv,char *fileName);
+	bool					LoadVotes();
 public:
-	CacheBuilder();
+	LinearVote();
 
 	bool					Init();
 	void					OnExecute();
--- code/Algorithms/LinearVote/src/LinearVote.cpp
+++ code/Algorithms/LinearVote/src/LinearVote.cpp
@@ -1,55 +1,154 @@
-#include "CacheBuilder.h"
+#include "LinearVote.h"
 
 
-CacheBuilder::CacheBuilder()
+PerceptronVector::PerceptronVector()
 {
-	ObjectName = "CacheBuilder";
+	Weight = NULL;
+	Bias = 0;
+	Count = 0;
+	Vote = 0;
+}
+PerceptronVector::~PerceptronVector()
+{
+	Destroy();
+}
+void PerceptronVector::Destroy()
+{
+	if (Weight!=NULL)
+		delete Weight;
+	Bias = 0;
+	Weight = NULL;
+	Bias = NULL;
+	Count = 0;
+	Vote = 0;
+}
+bool PerceptronVector::Create(UInt32 count)
+{
+	Destroy();
+	if ((Weight = new double[count])==NULL)
+		return false;
+	Bias = 0.0;
+	memset(Weight,0,sizeof(double)*count);
+	Vote = 0;
+	Count = count;
+
+	return true;
+}
+//================================================================================
+
+LinearVote::LinearVote()
+{
+	ObjectName = "LinearVote";
+
+	pVectors = NULL;
 
 	SetPropertyMetaData("Command","!!LIST:None=0,CreateCache!!");
-	LinkPropertyToString("CacheName",				CacheName				,"","Name of the file that will contain the cache");
 	LinkPropertyToString("DataBase"					,DataBase				,"");
 	LinkPropertyToString("Connector"				,Conector				,"");
 	LinkPropertyToString("Notifier"					,Notifier				,"");
+	LinkPropertyToString("WeightFiles"				,WeightFiles			,"","A list of weight files to be loaded separated by a comma.");
 }
-bool CacheBuilder::Init()
+bool LinearVote::Create(PerceptronVector &pv,char *fileName)
 {
-	if ((notif = GML::Builder::CreateNotifier(Notifier.GetText()))==NULL)
-		return false;
-	if (CacheName.Len()==0)
-	{
-		notif->Error("[%s] -> CacheName property is not set",ObjectName);
-		return false;
-	}
+	GML::Utils::AttributeList	attr;
 
-	if ((db = GML::Builder::CreateDataBase(DataBase.GetText(),*notif))==NULL)
+	if (pv.Create(con->GetFeatureCount())==false)
 	{
-		notif->Error("[%s] -> Unable to create Database (%s)",ObjectName,DataBase.GetText());
+		notif->Error("[%s] -> Unable to create PerceptronVector object ",ObjectName);
 		return false;
 	}
-	if (db->Connect()==false)
+	if (attr.Load(fileName)==false)
 	{
-		notif->Error("[%s] -> Unable to connesct to Database (%s)",ObjectName,DataBase.GetText());
+		notif->Error("[%s] -> Unable to load / Invalid format for %s ",ObjectName,fileName);
 		return false;
 	}
-	if ((con = GML::Builder::CreateConnectors(Conector.GetText(),*notif,*db))==NULL)
+	if (attr.UpdateDouble("Bias",pv.Bias)==false)
 	{
-		notif->Error("[%s] -> Unable to create Conector (%s)",ObjectName,Conector.GetText());
+		notif->Error("[%s] -> Missing 'Bias' field in %s ",ObjectName,fileName);
+		return false;
+	}
+	if (attr.Update("Weight",pv.Weight,sizeof(double)*con->GetFeatureCount())==false)
+	{
+		notif->Error("[%s] -> Missing 'Weight' field or different count of Weights in %s ",ObjectName,fileName);
+		return false;
+	}
+	if (pv.FileName.Set(fileName)==false)
+	{
+		notif->Error("[%s] -> Unable to set FileName property for PeceptronVector",ObjectName);
 		return false;
 	}
 	return true;
 }
-void CacheBuilder::OnExecute()
+bool LinearVote::LoadVotes()
+{
+	GML::Utils::GString		tmp;
+	int						poz,count;
+
+	if (WeightFiles.Len()==0)
+	{
+		notif->Error("[%s] -> You need to set up 'WeightFiles' property with a list of files !",ObjectName);
+		return false;
+	}
+
+	poz = 0;
+	count = 0;
+	while (WeightFiles.CopyNext(&tmp,";",&poz))
+	{
+		tmp.Strip();
+		if (tmp.Len()>0)
+			count++;
+	}
+	if (count==0)
+	{
+		notif->Error("[%s] -> You need to set up 'WeightFiles' property with a list of files !",ObjectName);
+		return false;
+	}
+
+	if ((pVectors = new PerceptronVector[count])==NULL)
+	{
+		notif->Error("[%s] -> Unable to allocate %d vectors !",ObjectName,count);
+		return false;
+	}
+
+	return true;
+}
+bool LinearVote::Init()
+{
+	if ((notif = GML::Builder::CreateNotifier(Notifier.GetText()))==NULL)
+		return false;
+	if (DataBase.Len()!=0)
+	{
+		if ((db = GML::Builder::CreateDataBase(DataBase.GetText(),*notif))==NULL)
+		{
+			notif->Error("[%s] -> Unable to create Database (%s)",ObjectName,DataBase.GetText());
+			return false;
+		}
+		if (db->Connect()==false)
+		{
+			notif->Error("[%s] -> Unable to connesct to Database (%s)",ObjectName,DataBase.GetText());
+			return false;
+		}
+		if ((con = GML::Builder::CreateConnectors(Conector.GetText(),*notif,*db))==NULL)
+		{
+			notif->Error("[%s] -> Unable to create Conector (%s)",ObjectName,Conector.GetText());
+			return false;
+		}
+	} else {
+		if ((con = GML::Builder::CreateConnectors(Conector.GetText(),*notif))==NULL)
+		{
+			notif->Error("[%s] -> Unable to create Conector (%s)",ObjectName,Conector.GetText());
+			return false;
+		}
+	}
+	return true;
+}
+void LinearVote::OnExecute()
 {
 	if (Command==1)	//CreateCache
 	{
 		if (con!=NULL)
 		{
-			if (con->Save(CacheName.GetText())==false)
-			{
-				notif->Error("[%s] -> Unable to save cache to %s",ObjectName,CacheName.GetText());
-			} else {
-				notif->Info("[%s] -> Cache created (%s)",ObjectName,CacheName.GetText());
-			}
+
 		}
 	}
 }
