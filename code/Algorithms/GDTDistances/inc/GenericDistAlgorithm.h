cf9d2b7d0c53ee62a5abd877671f853489f1d2b4
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-16 21:36:16 +0000

    GenericDistAlgoritgm swicth to IMLAlgorithm

--- code/Algorithms/GDTDistances/inc/GenericDistAlgorithm.h
+++ code/Algorithms/GDTDistances/inc/GenericDistAlgorithm.h
@@ -3,16 +3,14 @@
 
 #include "gmllib.h"
 
-struct ThreadData
+struct DistThreadData
 {
-	UInt32				ID;
-	GML::ML::MLRecord	Rec1,Rec2;
-
+	GML::ML::MLRecord	SetRec;
 };
 
 #define SAVE_CACHE_SIZE		1024
 
-class GenericDistAlgorithm : public GML::Algorithm::IAlgorithm
+class GenericDistAlgorithm : public GML::Algorithm::IMLAlgorithm
 {
 protected:
 	enum {
@@ -25,34 +23,22 @@ protected:
 	};
 
 	// proprietati
-	UInt32							threadsCount;
-	GML::DB::IDataBase				*db;
-	GML::ML::IConnector				*con;
-
-	// proprietati	
-	GML::Utils::GString				Conector;
-	GML::Utils::GString				DataBase;
-	GML::Utils::GString				Notifier;
 	GML::Utils::GString				HashFileName;
 	UInt32							HashStoreMethod;
 
-	GML::Utils::ThreadParalelUnit	*tpu;
 	GML::Utils::Indexes				indexesPozitive,indexesNegative;
 	GML::Utils::GTFVector<UInt8>	RecordsStatus;
-public:
-	ThreadData						*ptData;
 
 	
 public:
 	GenericDistAlgorithm();
 
 
-	virtual void					OnRunThreadCommand(ThreadData &td,UInt32 command) {};
+	bool							OnInitThreadData(GML::Algorithm::MLThreadData &thData);
 	virtual bool					OnInit();
 	virtual bool					OnCompute();
 
 	bool							SaveHashResult();
-	bool							ExecuteParalelCommand(UInt32 command);
 	bool							Init();
 	void							OnExecute();
 	bool							CreatePozitiveAndNegativeIndexes();
