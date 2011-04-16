be8fd15a3cde542fe68856bc7ffb3cfd25e8094f
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-16 15:53:39 +0000

    IMLAlgorithm -> Interface for generic ML Algorithms

--- trunk/code/GmlLib/inc/IMLAlgorithm.h
+++ trunk/code/GmlLib/inc/IMLAlgorithm.h
@@ -1,8 +1,32 @@
 #ifndef __INTERFACE_ML_ALGORITHM__
 #define __INTERFACE_ML_ALGORITHM__
 
-class IMLAlgorithm
+#include "IAlgorithm.h"
+#include "GString.h"
+#include "IDataBase.h"
+#include "MLInterface.h"
+#include "Builder.h"
+
+namespace GML
 {
-};
+	namespace Algorithm
+	{
+		class EXPORT IMLAlgorithm: public GML::Algorithm::IAlgorithm
+		{
+		protected:
+			UInt32					threadsCount;
+			GML::Utils::GString		Conector;
+			GML::Utils::GString		DataBase;
+			GML::Utils::GString		Notifier;
+			GML::DB::IDataBase		*db;
+			GML::ML::IConnector		*con;
+
+			bool					InitConnections();
+
+		public:
+			IMLAlgorithm();	
+		};
+	}
+}
 
 #endif
