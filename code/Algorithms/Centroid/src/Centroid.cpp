32dfbbd5c1e5b650115e4147914b04a4d766c232
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-24 20:05:38 +0000

    Centroid simple Algorithm

--- code/Algorithms/Centroid/src/Centroid.cpp
+++ code/Algorithms/Centroid/src/Centroid.cpp
@@ -1,12 +1,36 @@
 #include "Centroid.h"
 
+#define FIND_CENTROID	0
+
 
 Centroid::Centroid()
 {
 	ObjectName = "Centroid";
 
 	SetPropertyMetaData("Command","!!LIST:None=0,Train,Test!!");
-	
+
+		
+}
+void Centroid::OnRunThreadCommand(GML::Algorithm::MLThreadData &thData,UInt32 threadCommand)
+{
+	switch (threadCommand)
+	{
+		case FIND_CENTROID:
+			FindCentroid(thData,indexesPozitive,indexesPozitive,indexesNegative);
+			return;
+	}
+}
+bool Centroid::OnInitThreadData(GML::Algorithm::MLThreadData &thData)
+{
+	CentroidThreadData	*ctd = new CentroidThreadData();
+	if (ctd==NULL)
+		return false;
+
+	if (con->CreateMlRecord(ctd->SecRec)==false)
+		return false;
+
+	thData.Context = ctd;
+	return true;
 }
 bool Centroid::CreatePozitiveAndNegativeIndexes()
 {
@@ -65,18 +89,119 @@ bool Centroid::CreatePozitiveAndNegativeIndexes()
 }
 bool Centroid::Init()
 {
+	UInt32				tr;
+	CentroidDistances	cd;
+
 	if (InitConnections()==false)
 		return false;
 	if (CreatePozitiveAndNegativeIndexes()==false)
 		return false;
+	if (InitThreads()==false)
+		return false;
+	if (distInfo.Create(con->GetRecordCount())==false)
+	{
+		notif->Error("[%s] -> Unable to alloc %d elements for distInfo",ObjectName,con->GetRecordCount());
+		return false;
+	}
+	for (tr=0;tr<con->GetRecordCount();tr++)
+	{
+		cd.index = tr;
+		cd.corectelyClasify = 0;
+		cd.ClosestDifferentDistance = 0.0;
+		cd.MostDistantSimilarDistance = 0.0;
+		if (distInfo.PushByRef(cd)==false)
+		{
+			notif->Error("[%s] -> Unable to add elements for distInfo",ObjectName);
+			return false;
+		}
+	}
+	return true;
+}
+bool Centroid::FindCentroid(GML::Algorithm::MLThreadData &thData,GML::Utils::Indexes &indexWork,GML::Utils::Indexes &indexPoz,GML::Utils::Indexes &indexNeg)
+{
+	UInt32					tr,gr,featuresCount,sameClassCount,diffClassCount,workCount,idxWork,idx,countClasified;
+	CentroidThreadData*		dt = (CentroidThreadData *)thData.Context;
+	GML::Utils::Indexes		*indexSameClass,*indexDiffClass;
+	CentroidDistances*		cd = distInfo.GetVector();
+	double					dist,maxDiffDist,centroidRay;
+	
 
+	featuresCount = con->GetFeatureCount();
+	workCount = indexWork.Len();
+
+	if (thData.ThreadID==0)
+		notif->StartProcent("[%s] -> Computing ... ",ObjectName);
+	for (tr=thData.ThreadID;tr<workCount;tr+=threadsCount)
+	{
+		idxWork = indexWork.Get(tr);
+		if (con->GetRecord(thData.Record,idxWork)==false)
+		{
+			notif->Error("[%s] -> Unable to read record #%d",ObjectName,idxWork);
+			return false;
+		}
+		// caut cel mai apropiat de o clasa diferita
+		if (thData.Record.Label == 1)
+		{
+			indexSameClass = &indexPoz;
+			indexDiffClass = &indexNeg;
+		} else {
+			indexSameClass = &indexNeg;
+			indexDiffClass = &indexPoz;
+		}
+		sameClassCount = indexSameClass->Len();
+		diffClassCount = indexDiffClass->Len();
+		
+		// caut din clasa diferita cel mai apropiat
+		maxDiffDist = 10000000000;
+		for (gr=0;gr<diffClassCount;gr++)
+		{
+			idx = indexDiffClass->Get(tr);
+			if (con->GetRecord(dt->SecRec,idx)==false)
+			{
+				notif->Error("[%s] -> Unable to read record #%d",ObjectName,idx);
+				return false;
+			}
+			dist = GML::ML::VectorOp::PointToPointDistance(thData.Record.Features,dt->SecRec.Features,featuresCount);
+			if (dist<maxDiffDist)
+				maxDiffDist = dist;
+		}
+
+		// numar sa vad dintre cele similare cate sunt apropiate de el
+		countClasified = 0;
+		centroidRay = 0.0;
+		for (gr=0;gr<sameClassCount;gr++)
+		{
+			idx = indexSameClass->Get(tr);
+			if (con->GetRecord(dt->SecRec,idx)==false)
+			{
+				notif->Error("[%s] -> Unable to read record #%d",ObjectName,idx);
+				return false;
+			}
+			dist = GML::ML::VectorOp::PointToPointDistance(thData.Record.Features,dt->SecRec.Features,featuresCount);
+			if (dist<maxDiffDist)
+			{
+				countClasified++;
+				if (dist>centroidRay)
+					centroidRay = dist;
+			}
+			cd[tr].corectelyClasify = countClasified;
+			cd[tr].ClosestDifferentDistance = maxDiffDist;
+			cd[tr].MostDistantSimilarDistance = centroidRay;
+		}
+
+		// salvez cate ar putea clasifica
+		if (thData.ThreadID==0)
+			notif->SetProcent(tr,workCount);
+	}
+	if (thData.ThreadID==0)
+		notif->EndProcent();
 	return true;
 }
 void Centroid::OnExecute()
 {
 	if (Command==1)	//Train
 	{
-
+		ExecuteParalelCommand(FIND_CENTROID);
 	}
 	notif->Error("[%s] -> Unknown command ID: %d",ObjectName,Command);
 }
