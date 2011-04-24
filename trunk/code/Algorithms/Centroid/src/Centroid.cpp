60648f48bdb8830560ccdcf410cd26fda1bf5920
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-24 18:49:15 +0000

    Centroid Algorithm

---
+++ trunk/code/Algorithms/Centroid/src/Centroid.cpp
@@ -0,0 +1,82 @@
+#include "Centroid.h"
+
+
+Centroid::Centroid()
+{
+	ObjectName = "Centroid";
+
+	SetPropertyMetaData("Command","!!LIST:None=0,Train,Test!!");
+	
+}
+bool Centroid::CreatePozitiveAndNegativeIndexes()
+{
+	UInt32	tr;
+	UInt32	pozitiveCount,negativeCount,nrRecords;
+	double	label;
+	bool	res;
+
+	notif->Info("[%s] -> Creating Pozitive and Negative indexes ... ",ObjectName);
+	pozitiveCount = negativeCount = 0;
+	nrRecords = con->GetRecordCount();
+	
+	for (tr=0;tr<nrRecords;tr++)
+	{
+		if (con->GetRecordLabel(label,tr)==false)
+		{
+			notif->Error("[%s] -> Unable to read label for record #%d",ObjectName,tr);
+			return false;
+		}
+		if (label==1.0)
+			pozitiveCount++;
+		else
+			negativeCount++;
+	}
+	notif->Info("[%s] -> Pozitive = %d ,Negative = %d",ObjectName,pozitiveCount,negativeCount);
+	if (indexesPozitive.Create(pozitiveCount)==false)
+	{
+		notif->Error("[%s] -> Unable to alloc %d pozitive indexes",ObjectName,pozitiveCount);
+		return false;
+	}
+	if (indexesNegative.Create(negativeCount)==false)
+	{
+		notif->Error("[%s] -> Unable to alloc %d negative indexes",ObjectName,negativeCount);
+		return false;
+	}
+	// adaugam si indexii
+	for (tr=0;tr<nrRecords;tr++)
+	{
+		if (con->GetRecordLabel(label,tr)==false)
+		{
+			notif->Error("[%s] -> Unable to read label for record #%d",ObjectName,tr);
+			return false;
+		}
+		if (label==1.0)
+			res = indexesPozitive.Push(tr);
+		else
+			res = indexesNegative.Push(tr);
+		if (res==false)
+		{
+			notif->Error("[%s] -> Unable to add index #%d",ObjectName,tr);
+			return false;
+		}		
+	}
+	notif->Info("[%s] -> Pozitive and Negative indexes created !",ObjectName);
+	return true;
+}
+bool Centroid::Init()
+{
+	if (InitConnections()==false)
+		return false;
+	if (CreatePozitiveAndNegativeIndexes()==false)
+		return false;
+
+	return true;
+}
+void Centroid::OnExecute()
+{
+	if (Command==1)	//Train
+	{
+
+	}
+	notif->Error("[%s] -> Unknown command ID: %d",ObjectName,Command);
+}
