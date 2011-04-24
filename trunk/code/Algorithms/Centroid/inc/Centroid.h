f0d7c3291ad2fd40bead0254c35a97e88e6f1621
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-04-24 20:05:38 +0000

    Centroid simple Algorithm

--- trunk/code/Algorithms/Centroid/inc/Centroid.h
+++ trunk/code/Algorithms/Centroid/inc/Centroid.h
@@ -3,11 +3,30 @@
 
 #include "gmllib.h"
 
+struct CentroidDistances
+{
+	UInt32	index;
+	UInt32	corectelyClasify;
+	double	MostDistantSimilarDistance;
+	double	ClosestDifferentDistance;
+};
+struct CentroidThreadData
+{
+	GML::ML::MLRecord				SecRec;
+};
+
 class Centroid: public GML::Algorithm::IMLAlgorithm
 {
-	GML::Utils::Indexes		indexesPozitive,indexesNegative;
+	GML::Utils::Indexes							indexesPozitive,indexesNegative;
+	GML::Utils::GTFVector<CentroidDistances>	distInfo;
 
 	bool					CreatePozitiveAndNegativeIndexes();
+
+	void					OnRunThreadCommand(GML::Algorithm::MLThreadData &thData,UInt32 threadCommand);
+	bool					OnInitThreadData(GML::Algorithm::MLThreadData &thData);
+
+	bool					FindCentroid(GML::Algorithm::MLThreadData &thData,GML::Utils::Indexes &indexWork,GML::Utils::Indexes &indexPoz,GML::Utils::Indexes &indexNeg);
+
 public:
 	Centroid();
 
