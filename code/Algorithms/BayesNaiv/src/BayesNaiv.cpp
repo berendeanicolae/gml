33d6deb3eac758c3288f629187f54f6d19aa3790
marius.barat <marius.barat@gmail.com>
2011-05-10 18:05:52 +0000

    BayesNaiv v2.0

--- code/Algorithms/BayesNaiv/src/BayesNaiv.cpp
+++ code/Algorithms/BayesNaiv/src/BayesNaiv.cpp
@@ -1,4 +1,4 @@
-#include "BayesNaiv.h"
+	#include "BayesNaiv.h"
 
 BayesNaiv::BayesNaiv()
 {
@@ -10,15 +10,24 @@ BayesNaiv::BayesNaiv()
 	LinkPropertyToDouble("ProcToSetVerdictInfected",procToSetInfected,0.999,"MinProbability to set the verdict=infected");
 	LinkPropertyToBool("ClassicMul", classicMul, true, "Use normal multiplication method for probabilities");	
 	LinkPropertyToString("ProbsFilePath", pathToProbsFile, "", "Path to the file to save the probabilities in");	
-	LinkPropertyToString("PathTrainedProbsFile", pathTrainedProbsFile, "", "Path to the file to save the trained probabilities in");
-	LinkPropertyToBool("SaveTrainedProbs", saveTrainedProbs, false, "Save the trained probabilities");
+	LinkPropertyToString("PathBestProbsFile", pathBestProbsFile, "", "Path to the file to save the best probabilities in");
+	LinkPropertyToBool("SaveBest", saveBest, false, "Save the trained probabilities");
+	LinkPropertyToUInt32("BestClassifMethod", bestClassifMethod, BEST_NONE, "!!LIST:None=0,BEST_SE,BEST_SP,BEST_ACC!!");
 	
 	SetPropertyMetaData("Command","!!LIST:None=0,Train,Test!!");
 }
 
 bool BayesNaiv::Init()
 {
-	return InitConnections();
+	if (InitConnections()==false)
+		return false;	
+
+	pFeatCondInf = new double[con->GetFeatureCount()];
+	pFeatCondClean = new double[con->GetFeatureCount()];
+	for(int i=0; i<con->GetFeatureCount(); i++)
+		pFeatCondInf[i] = pFeatCondClean[i] = 1.0;
+
+	return InitThreads();
 }
 
 unsigned int first4Decimals(double value)
@@ -63,184 +72,74 @@ unsigned int mulPlusTrunc(unsigned int a, unsigned int b, unsigned int &exponent
 	return prod;	
 }
 
-bool BayesNaiv::PerformTestClassicMul()
+bool BayesNaiv::BuildRecordsClassif(GML::Algorithm::MLThreadData &thData)
 {
-	GML::Utils::AttributeList	a;
-
-	double						*pFeatCondInf;
-	double						*pFeatCondClean;
-	double						pFileInf, pFileClean;
-	unsigned int				i, j;
-	unsigned int				*vcFNs;
-	unsigned int				*vcFNsC;
-	unsigned int				*vcFPs;
-	unsigned int				*vcFPsC;
-	int							clasif;
-	GML::Utils::AlgorithmResult	result;
 	GML::ML::MLRecord			currentRecord;
+	int							i,j;
+	int							clasif;
 
 	if(!(con->CreateMlRecord(currentRecord)))
 		return false;	
 
-	if(pathToProbsFile.Equals(""))
+	for(i=thData.ThreadID; i<con->GetRecordCount(); i+=threadsCount)
 	{
-		notif->Error("[%s] Invalid path for file to load probabilities from", ObjectName);
-		return false;
-	}
-	if(saveTrainedProbs && pathTrainedProbsFile.Equals(""))
-	{
-		notif->Error("[%s] Invalid path for file to save trained probabilities from", ObjectName);
-		return false;
-	}
-
-	pFeatCondInf = new double[con->GetFeatureCount()];
-	pFeatCondClean = new double[con->GetFeatureCount()];
-
-	vcFNs = new unsigned int[con->GetFeatureCount()];
-	vcFPs = new unsigned int[con->GetFeatureCount()];
-	vcFNsC = new unsigned int[con->GetFeatureCount()];
-	vcFPsC = new unsigned int[con->GetFeatureCount()];
-
-	for(i=0; i<con->GetFeatureCount(); i++)
-			vcFNsC[i] = vcFPsC[i] = 0;
-
-	a.Load(pathToProbsFile);
-	a.UpdateDouble("Pinf", pFileInf,true, 0.5);
-	a.UpdateDouble("Pclean", pFileClean,true, 0.5);
-	a.Update("pFeatCondClean", &pFeatCondClean[0], con->GetFeatureCount()*sizeof(double));
-	a.Update("pFeatCondInf", &pFeatCondInf[0], con->GetFeatureCount()*sizeof(double));	
-
-	a.Clear();
-	bestIteration = 0.0;
-
-	int copyOfNrIter = nrOfIterations;
-	while(copyOfNrIter--)
-	{
-		result.Clear();
-		result.time.Start();
-
-		for(i=0; i<con->GetFeatureCount(); i++)
-			vcFNs[i] = vcFPs[i] = 0;
-
-		for(i=0; i<con->GetRecordCount(); i++)
+		if(!(con->GetRecord(currentRecord, i)))
 		{
-			if(!(con->GetRecord(currentRecord, i)))
-			{
-				notif->Info("[%s] -> error getting record #%d",ObjectName,i);
-				return false;
-			}
-
-			double pInf		= pFileInf*penaltyForInfFile;
-			double pClean	= pFileClean;							
-
-			for(j=0; j<currentRecord.FeatCount; j++)
-				if((unsigned int)currentRecord.Features[j])
-				{								
-					pInf *= pFeatCondInf[j];
-					pClean *= pFeatCondClean[j];
-				}
-
-			clasif = -1;
-			if(pInf>pClean)
-				if((double)pInf/(pInf+pClean) > procToSetInfected)
-					clasif = 1;		
+			notif->Info("[%s] -> error getting record #%d",ObjectName,i);
+			return false;
+		}
 		
-			result.Update(currentRecord.Label==1,clasif==currentRecord.Label);
+		double pInf		= pFileInf*penaltyForInfFile;
+		double pClean	= pFileClean;							
 
-			if(clasif != currentRecord.Label)
-			{				
-				if(clasif == 1)		//fp
-				{
-					for(j=0; j<currentRecord.FeatCount; j++)					
-						if((unsigned int)currentRecord.Features[j])
-							vcFPs[j]++;
-				}
-				else				//fn
-				{
-					for(j=0; j<currentRecord.FeatCount; j++)					
-						if((unsigned int)currentRecord.Features[j])
-							vcFNs[j]++;
-				}
+		for(j=0; j<currentRecord.FeatCount; j++)
+			if((unsigned int)currentRecord.Features[j])
+			{								
+				pInf *= pFeatCondInf[j];
+				pClean *= pFeatCondClean[j];
 			}
-		}
 
-		result.Compute();
-		result.Iteration = nrOfIterations - copyOfNrIter - 1;
-		result.time.Stop();
-		notif->Result(result);
+		clasif = -1;		
+		if(pInf>pClean)
+			if((double)pInf/(pInf+pClean) > procToSetInfected)
+				clasif = 1;									
+		
+		result.Update(currentRecord.Label==1,clasif==currentRecord.Label);
 
-		if(saveTrainedProbs)
-		{
-			a.Clear();
-			a.AddAttribute("pFeatCondClean",&pFeatCondClean[0],GML::Utils::AttributeList::DOUBLE,con->GetFeatureCount(),"Conditional probabilities for clean files");
-			a.AddAttribute("pFeatCondInf",&pFeatCondInf[0],GML::Utils::AttributeList::DOUBLE,con->GetFeatureCount(),"Conditional probabilities for infected files");
-			a.AddDouble("Pinf", pFileInf, "Prob for infected file");
-			a.AddDouble("Pclean", pFileClean, "Prob for clean files");		
-			a.AddDouble("Se", result.se);
-			a.AddDouble("Sp", result.sp);
-			a.AddDouble("Acc", result.acc);
-			GML::Utils::GString temp;
-			temp.Add(pathTrainedProbsFile);
-			temp.AddFormated("%d", result.Iteration);
-			a.Save(temp);	
-
-			if(bestIteration < result.sp)
-			{
-				temp.Set(pathTrainedProbsFile);
-				temp.Add("BEST");
-				a.Save(temp);
-			}
-		}
-
-		int maxApFPs		= 0;		
-		int maxApFPsIndex	= 0;		
-		int maxApFNs		= 0;		
-		int maxApFNsIndex	= 0;		
-		for(i=0; i<con->GetFeatureCount(); i++)
-		{
-			if((unsigned int)pFeatCondInf[i] == 1 || (unsigned int)pFeatCondClean[i] == 1)
-				continue;
-
-			if(maxApFPs < vcFPs[i])
-			{
-				maxApFPs = vcFPs[i];
-				maxApFPsIndex = i;
-			}
-			if(maxApFNs < vcFNs[i])
-			{
-				maxApFNs = vcFNs[i];
-				maxApFNsIndex = i;
-			}			
-		}
-				
-		//pFeatCondInf[maxApFPsIndex] *= 0.8;
-		//pFeatCondClean[maxApFNsIndex] *= 0.9;
-		vcFPsC[maxApFPsIndex]++;
-		vcFNsC[maxApFNsIndex]++;
-		if(vcFPsC[maxApFPsIndex]>5)
-		{
-			pFeatCondInf[maxApFPsIndex] = 1.0;
-			pFeatCondClean[maxApFPsIndex] = 1.0;
-		}
-		else if(vcFNsC[maxApFNsIndex]>5)
-		{
-			pFeatCondInf[maxApFNsIndex] = 1.0;
-			pFeatCondClean[maxApFNsIndex] = 1.0;
-		}
-		else
-		{
-			//pFeatCondInf[maxApFPsIndex] -= (double)vcFPs[maxApFPsIndex]/(result.tp+result.fp);
-			//pFeatCondClean[maxApFNsIndex] -= (double)vcFNs[maxApFNsIndex]/(result.tn+result.fn);
-			//pFeatCondClean[maxApFPsIndex] += (double)vcFPs[maxApFPsIndex]/(result.tp+result.fp);
-			//pFeatCondInf[maxApFNsIndex] += (double)vcFNs[maxApFNsIndex]/(result.tn+result.fn);
-			if(vcFPs[maxApFPsIndex]/(result.fp) > vcFNs[maxApFNsIndex]/(result.fn))
-				//pFeatCondClean[maxApFPsIndex] += (double)vcFPs[maxApFPsIndex]/(result.fp);
-				pFeatCondInf[maxApFPsIndex] *= 0.9;
+		if(currentRecord.Label == 1)
+			if(clasif == 1)
+				recordsClassif[i] = TRUE_POSITIVE;
 			else
-				//pFeatCondInf[maxApFNsIndex] += (double)vcFNs[maxApFNsIndex]/(result.fn);
-				pFeatCondClean[maxApFPsIndex] *= 0.9;
-		}		
+				recordsClassif[i] = FALSE_NEGATIVE;
+		else
+			if(clasif == 1)
+				recordsClassif[i] = FALSE_POSITIVE;
+			else
+				recordsClassif[i] = TRUE_NEGATIVE;
 	}
+
+	return true;
+}
+
+bool BayesNaiv::PerformTestClassicMul(bool toLoad, bool alloc, int iterNumber)
+{
+	result.Clear();
+	result.time.Start();
+
+	if(toLoad)
+		LoadProbsFromFile();
+
+	if(alloc)
+		recordsClassif = new UInt8[con->GetRecordCount()];
+
+	memset(recordsClassif, 0, sizeof(UInt8)*(con->GetRecordCount()));	
+	ExecuteParalelCommand(Command);
+
+	result.Compute();	
+	result.time.Stop();
+	result.Iteration = iterNumber;
+	notif->Result(result);
+
 	return true;
 }
 
@@ -324,22 +223,8 @@ bool BayesNaiv::PerformTestNewMul()
 	return true;
 }
 
-bool BayesNaiv::PerformTrain()
+bool BayesNaiv::InitTrain()
 {
-	unsigned int		*vcInf;
-	unsigned int		*vcClean;
-	unsigned int		totalInf, totalClean;
-	unsigned int		nTotal;
-	unsigned int		i,j;
-	double				*pFeatCondInf;
-	double				*pFeatCondClean;
-	
-	GML::ML::MLRecord currentRecord;
-	vcInf = new unsigned int[con->GetFeatureCount()];
-	vcClean = new unsigned int[con->GetFeatureCount()];
-	pFeatCondInf = new double[con->GetFeatureCount()];
-	pFeatCondClean = new double[con->GetFeatureCount()];
-
 	if (procIgnoreFeature>100)
 	{
 		notif->Error("[%s] Inavlid procent value (%d) ",ObjectName,procIgnoreFeature);
@@ -352,18 +237,192 @@ bool BayesNaiv::PerformTrain()
 		return false;
 	}
 
+	vcInf = new unsigned int[con->GetFeatureCount()];
+	vcClean = new unsigned int[con->GetFeatureCount()];
+	
+	memset(vcInf, 0, sizeof(unsigned int)*con->GetFeatureCount());
+	memset(vcClean, 0, sizeof(unsigned int)*con->GetFeatureCount());
+
+	totalClean = totalInf = 0;
+
+	return true;
+}
+
+bool BayesNaiv::PerformTrain()
+{
+	InitTrain();
+	ExecuteParalelCommand(Command);
+	//notif->Info("[%s] -> totalClean=%d totalInf=%d totalRecords=%d", ObjectName, totalClean, totalInf, con->GetRecordCount());
+	BuildInitialProbabilities();	
+
+	pFileInf = (double)totalInf/(totalInf + totalClean);
+	pFileClean = (double)totalClean/(totalInf + totalClean);
+	
+	Command = COMMAND_TEST;
+	PerformTestClassicMul(false, true);
+	SaveProbsToFile(pathToProbsFile);
+
+	unsigned int		*vcFNs;
+	unsigned int		*vcFPs;
+	int					copyOfNrIter = nrOfIterations;
+	int					i;
+
+	if(nrOfIterations>1)
+	{
+		vcFNs = vcInf;
+		vcFPs = vcClean;
+		vcFNsC = new UINT8[con->GetFeatureCount()];
+		vcFPsC = new UINT8[con->GetFeatureCount()];
+		memset(vcFNsC, 0, sizeof(UINT8)*con->GetFeatureCount());
+		memset(vcFPsC, 0, sizeof(UINT8)*con->GetFeatureCount());
+
+		bestIteration = 0.0;
+	}
+
+	while(--copyOfNrIter)
+	{
+		memset(vcFNs, 0, sizeof(unsigned int)*con->GetFeatureCount());
+		memset(vcFPs, 0, sizeof(unsigned int)*con->GetFeatureCount());
+
+		Command = COMMAND_COMPUTE_NEW_PROBS;
+		ExecuteParalelCommand(Command);
+
+		int maxApFPs		= 0;		
+		int maxApFPsIndex	= 0;		
+		int maxApFNs		= 0;		
+		int maxApFNsIndex	= 0;		
+		for(i=0; i<con->GetFeatureCount(); i++)
+		{
+			if((unsigned int)pFeatCondInf[i] == 1 || (unsigned int)pFeatCondClean[i] == 1)
+				continue;
+
+			if(maxApFPs < vcFPs[i])
+			{
+				maxApFPs = vcFPs[i];
+				maxApFPsIndex = i;
+			}
+			if(maxApFNs < vcFNs[i])
+			{
+				maxApFNs = vcFNs[i];
+				maxApFNsIndex = i;
+			}			
+		}
+
+		//pFeatCondInf[maxApFPsIndex] *= 0.8;
+		//pFeatCondClean[maxApFNsIndex] *= 0.9;
+		vcFPsC[maxApFPsIndex]++;
+		vcFNsC[maxApFNsIndex]++;
+		if(vcFPsC[maxApFPsIndex]>5)
+		{
+			pFeatCondInf[maxApFPsIndex] = 1.0;
+			pFeatCondClean[maxApFPsIndex] = 1.0;
+		}
+		else if(vcFNsC[maxApFNsIndex]>5)
+		{
+			pFeatCondInf[maxApFNsIndex] = 1.0;
+			pFeatCondClean[maxApFNsIndex] = 1.0;
+		}
+		else
+		{
+			//pFeatCondInf[maxApFPsIndex] -= (double)vcFPs[maxApFPsIndex]/(result.tp+result.fp);
+			//pFeatCondClean[maxApFNsIndex] -= (double)vcFNs[maxApFNsIndex]/(result.tn+result.fn);
+			//pFeatCondClean[maxApFPsIndex] += (double)vcFPs[maxApFPsIndex]/(result.tp+result.fp);
+			//pFeatCondInf[maxApFNsIndex] += (double)vcFNs[maxApFNsIndex]/(result.tn+result.fn);
+			if(vcFPs[maxApFPsIndex]/(result.fp) > vcFNs[maxApFNsIndex]/(result.fn))
+				//pFeatCondClean[maxApFPsIndex] += (double)vcFPs[maxApFPsIndex]/(result.fp);
+				pFeatCondInf[maxApFPsIndex] *= 0.7;
+			else
+				//pFeatCondInf[maxApFNsIndex] += (double)vcFNs[maxApFNsIndex]/(result.fn);
+				pFeatCondClean[maxApFNsIndex] *= 0.7;
+		}
+
+		Command = COMMAND_TEST;
+		PerformTestClassicMul(false, false, nrOfIterations - copyOfNrIter);
+		if(saveBest)
+			SaveProbsIfBetter(&bestIteration);		
+	}
+	
+	return true;
+}
+
+void BayesNaiv::SaveProbsIfBetter(double *best)
+{
+	double currentValue = 0.0;
+
+	switch(bestClassifMethod)
+	{
+	case BEST_SE:
+		currentValue = result.se;
+		break;
+	case BEST_SP:	
+		currentValue = result.sp;
+		break;
+	case BEST_ACC:
+		currentValue = result.acc;
+		break;
+	}
+
+	if(*best < currentValue)
+	{
+		*best = currentValue; 
+		GML::Utils::GString temp = pathBestProbsFile;
+		notif->Info("----> %s", temp);
+		SaveProbsToFile(temp);
+		temp.Set("");
+	}
+}
+
+bool BayesNaiv::ComputeWrongFeaturesFreq(GML::Algorithm::MLThreadData &thData)
+{
+	GML::ML::MLRecord			currentRecord;
+	int							i,j;
+
+	if(!(con->CreateMlRecord(currentRecord)))
+		return false;	
+
+	for(i=thData.ThreadID; i<con->GetRecordCount(); i+=threadsCount)
+	{
+		if(recordsClassif[i] == FALSE_POSITIVE)
+		{
+			if(!(con->GetRecord(currentRecord, i)))
+			{
+				notif->Info("[%s] -> error getting record #%d",ObjectName,i);
+				return false;
+			}
+
+			for(j=0; j<currentRecord.FeatCount; j++)					
+				if((unsigned int)currentRecord.Features[j])
+					vcClean[j]++;
+		} else if(recordsClassif[i] == FALSE_NEGATIVE)
+		{
+			if(!(con->GetRecord(currentRecord, i)))
+			{
+				notif->Info("[%s] -> error getting record #%d",ObjectName,i);
+				return false;
+			}
+
+			for(j=0; j<currentRecord.FeatCount; j++)					
+				if((unsigned int)currentRecord.Features[j])
+					vcInf[j]++;
+		}
+	}
+
+	return true;
+}
+
+bool BayesNaiv::ComputeFeaturesFreq(GML::Algorithm::MLThreadData &thData)
+{		
+	unsigned int		i,j;
+		
+	GML::ML::MLRecord currentRecord;
+	
 	if(!(con->CreateMlRecord(currentRecord)))
 		return false;
 
-	for(i=0; i<con->GetFeatureCount(); i++)
-	{
-		vcInf[i] = vcClean[i] = 0;
-		pFeatCondInf[i] = pFeatCondClean[i] = 1.0;
-	}
-	totalInf = totalClean = 0;	
-	nTotal = 0;
+	if (thData.ThreadID==0)
+		notif->StartProcent("[%s] -> Computing ... ",ObjectName);
 
-	for(i=0; i<con->GetRecordCount(); i++)
+	for(i=thData.ThreadID; i<con->GetRecordCount(); i+=threadsCount)
 	{
 		if(!(con->GetRecord(currentRecord, i)))
 		{
@@ -373,47 +432,87 @@ bool BayesNaiv::PerformTrain()
 		
 		if(currentRecord.Label == -1)	//clean
 		{
-			totalClean++;			
+			totalClean++;
 
 			for(j=0; j<currentRecord.FeatCount; j++)			
 				if((unsigned int)(currentRecord.Features[j]))	
 					vcClean[j]++;							
 		}
-		else if(currentRecord.Label == 1){		//infected
+		else if(currentRecord.Label == 1)	//infected
+		{		
 			totalInf++;
-			
+
 			for(j=0; j<currentRecord.FeatCount; j++)
 				if((unsigned int)(currentRecord.Features[j]))	
 					vcInf[j]++;							
-		}
-	}	
+		} 
 
-	for(i=0; i<con->GetFeatureCount(); i++)
-	{		
-		if(vcClean[i] || vcInf[i])	
-			nTotal++;		
+		if (thData.ThreadID==0)
+			notif->SetProcent(i,con->GetRecordCount());
 	}
-	UInt32	flagValueToIgnore = (con->GetRecordCount()*procIgnoreFeature)/100;
-	for(i=0; i<con->GetFeatureCount(); i++)
+
+	if (thData.ThreadID==0)
+		notif->EndProcent();
+
+	return true;
+}
+
+void BayesNaiv::SaveProbsToFile(GML::Utils::GString filePath)
+{
+	GML::Utils::AttributeList	a;
+
+	a.AddDouble("Pinf", pFileInf, "Prob for infected file");
+	a.AddDouble("Pclean", pFileClean, "Prob for clean files");
+	a.AddAttribute("pFeatCondClean",&pFeatCondClean[0],GML::Utils::AttributeList::DOUBLE,con->GetFeatureCount(),"Conditional probabilities for clean files");
+	a.AddAttribute("pFeatCondInf",&pFeatCondInf[0],GML::Utils::AttributeList::DOUBLE,con->GetFeatureCount(),"Conditional probabilities for infected files");
+	a.AddDouble("Se", result.se, "Se");
+	a.AddDouble("Sp", result.sp, "Sp");
+	a.AddDouble("Acc", result.acc, "Acc");
+
+	a.Save(filePath);	
+}
+
+void BayesNaiv::LoadProbsFromFile()
+{
+	if(pathToProbsFile.Equals(""))
 	{
-		if(vcClean[i] || vcInf[i])		
-		{			
-			if(vcClean[i] + vcInf[i] <  flagValueToIgnore)
-			{
-				pFeatCondClean[i] = (vcClean[i] + 1.0)/(totalClean + nTotal);
-				pFeatCondInf[i] = (vcInf[i] + 1.0)/(totalInf + nTotal);				
-			}
-		}
+		notif->Error("[%s] Invalid path for file to load probabilities from", ObjectName);		
 	}
 
 	GML::Utils::AttributeList	a;
 
-	a.AddDouble("Pinf", (double)totalInf/con->GetRecordCount(), "Prob for infected file");
-	a.AddDouble("Pclean", (double)totalClean/con->GetRecordCount(), "Prob for clean files");
-	a.AddAttribute("pFeatCondClean",&pFeatCondClean[0],GML::Utils::AttributeList::DOUBLE,con->GetFeatureCount(),"Conditional probabilities for clean files");
-	a.AddAttribute("pFeatCondInf",&pFeatCondInf[0],GML::Utils::AttributeList::DOUBLE,con->GetFeatureCount(),"Conditional probabilities for infected files");
+	a.Load(pathToProbsFile);
+	a.UpdateDouble("Pinf", pFileInf,true, 0.5);
+	a.UpdateDouble("Pclean", pFileClean,true, 0.5);
+	a.Update("pFeatCondClean", &pFeatCondClean[0], con->GetFeatureCount()*sizeof(double));
+	a.Update("pFeatCondInf", &pFeatCondInf[0], con->GetFeatureCount()*sizeof(double));	
+}
 
-	a.Save(pathToProbsFile);	
+bool BayesNaiv::BuildInitialProbabilities()
+{
+	unsigned int		i;
+	unsigned int		nTotal;
+	
+	nTotal = 0;
+
+	for(i=0; i<con->GetFeatureCount(); i++)
+	{		
+		if(vcClean[i] || vcInf[i])	
+			nTotal++;				
+	}
+	UInt32	flagValueToIgnore = (con->GetRecordCount()*procIgnoreFeature)/100;
+	for(i=0; i<con->GetFeatureCount(); i++)
+	{
+		
+		if(vcClean[i] || vcInf[i])		
+		{			
+			if(vcClean[i] + vcInf[i] <  flagValueToIgnore)
+			{				
+				pFeatCondClean[i] = (vcClean[i] + 1.0)/(con->GetFeatureCount() + nTotal);
+				pFeatCondInf[i] = (vcInf[i] + 1.0)/(con->GetFeatureCount() + nTotal);		
+			}
+		}		
+	}
 
 	return true;
 }
@@ -426,17 +525,33 @@ void BayesNaiv::OnExecute()
 			notif->Info("[%s] -> Nothing to do ... ",ObjectName);
 			return;
 		case COMMAND_TRAIN:
-			notif->Info("[%s] -> Training",ObjectName);
+			notif->Info("[%s] -> Training",ObjectName);		
 			PerformTrain();
 			return;
 		case COMMAND_TEST:
 			notif->Info("[%s] -> Testing",ObjectName);
 			if(classicMul)
-				PerformTestClassicMul();
+				PerformTestClassicMul(true, true);
 			else
 				PerformTestNewMul();
 			return;
 	};
 	notif->Error("[%s] -> Unkwnown command ID : %d",ObjectName,Command);
 	
+}
+
+void BayesNaiv::OnRunThreadCommand(GML::Algorithm::MLThreadData &thData,UInt32 threadCommand)
+{
+	switch(Command)
+	{
+		case COMMAND_TRAIN:
+			ComputeFeaturesFreq(thData);
+			break;
+		case COMMAND_TEST:
+			BuildRecordsClassif(thData);
+			break;
+		case COMMAND_COMPUTE_NEW_PROBS:
+			ComputeWrongFeaturesFreq(thData);
+			break;
+	}
 }
