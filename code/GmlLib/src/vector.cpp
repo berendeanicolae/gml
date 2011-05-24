fd4a827b2349dd5987bf69a4b6f0d354a96bd796
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-05-24 11:57:42 +0000

    Switch cu HeapSort :D

--- code/GmlLib/src/vector.cpp
+++ code/GmlLib/src/vector.cpp
@@ -148,6 +148,71 @@ void __BubbleSort(unsigned char *Data,int ElementSize,_BinarySearchCompFunction 
 		} while (!sorted);
 	}
 }
+//void __HeapSort(int arr[], unsigned int N) 
+void __HeapSort(unsigned char *Data,int ElementSize,_BinarySearchCompFunction cmpFunc,int nrElements,bool ascendent) 
+{
+    unsigned char	*tempElement;	// echivalentul lui t
+	unsigned char	tempData[1024];
+    unsigned int	n = nrElements, parent = nrElements/2, index, child; 
+
+	if (ElementSize<=1024)
+	{
+		tempElement = tempData;
+	} else {
+		if ((tempElement = new unsigned char[ElementSize])==NULL)
+			return;
+	}
+
+    /* loop until array is sorted */
+    for (;;) { 
+        if (parent > 0) { 
+			memcpy(tempElement,&Data[(--parent)*ElementSize],ElementSize);
+            /* first stage - Sorting the heap */
+            //t = arr[--parent];  /* save old value to t */
+        } else {
+            /* second stage - Extracting elements in-place */
+            n--;                /* make the heap smaller */
+            if (n == 0) return; /* When the heap is empty, we are done */
+            memcpy(tempElement,&Data[n*ElementSize],ElementSize);
+			memcpy(&Data[n*ElementSize],&Data[0],ElementSize);
+			//t = arr[n];         /* save lost heap entry to temporary */
+            //arr[n] = arr[0];    /* save root entry beyond heap */
+        }
+        /* insert operation - pushing t down the heap to replace the parent */
+        index = parent; /* start at the parent index */
+        child = index * 2 + 1; /* get its left child index */
+        while (child < n) {
+            /* choose the largest child */
+            //if (child + 1 < n  &&  arr[child + 1] > arr[child]) {
+            //    child++; /* right child exists and is bigger */
+            //}
+			if ((child + 1 < n) && (cmpFunc(&Data[(child+1)*ElementSize],&Data[child*ElementSize])>0))
+				child++;
+
+			if (cmpFunc(&Data[child*ElementSize],tempElement)>0)
+			{
+				memcpy(&Data[ElementSize*index],&Data[ElementSize*child],ElementSize);
+                index = child; 
+                child = index * 2 + 1; 
+			} else {
+				break;
+			}
+            /* is the largest child larger than the entry? */
+            //if (arr[child] > t) {
+            //    arr[index] = arr[child]; /* overwrite entry with child */
+            //    index = child; /* move index to the child */
+            //    child = index * 2 + 1; /* get the left child and go around again */
+            //} else {
+            //    break; /* t's place is found */
+            //}
+        }
+        /* store the temporary value at its new location */
+		memcpy(&Data[index*ElementSize],tempElement,ElementSize);
+        //arr[index] = t; 
+    }
+	if (ElementSize>1024)
+		delete tempElement;
+}
 //===================================================================================================================
 GML::Utils::Vector::Vector(void)
 {
@@ -301,5 +366,6 @@ void GML::Utils::Vector::Sort(_BinarySearchCompFunction cmpFunc,bool ascendet)
 {
 	if (NrElemente>0)
 		//__QSort(Data,ElementSize,cmpFunc,0,NrElemente-1,ascendet);
-		__BubbleSort(Data,ElementSize,cmpFunc,NrElemente,ascendet);
+		//__BubbleSort(Data,ElementSize,cmpFunc,NrElemente,ascendet);
+		__HeapSort(Data,ElementSize,cmpFunc,NrElemente,ascendet);
 }
