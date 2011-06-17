e8ae09f17139d5938c564d71d6576c228a113490
razvanblink@gmail.com <razvanblink@gmail.com@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-06-17 19:20:42 +0000

    Fixed BListCtrl bugs

--- trunk/prj/GMLGui/interfaceNotifier/BListCtrl.cpp
+++ trunk/prj/GMLGui/interfaceNotifier/BListCtrl.cpp
@@ -4,7 +4,6 @@
 IMPLEMENT_DYNAMIC(BListCtrl,CListCtrl)
 BListCtrl::BListCtrl()
 {
-	m_controlItems.Create(32, sizeof(ControlItem));
 	simpleFont.CreateFontA(12,0,0,0,FW_REGULAR,FALSE,FALSE,0,DEFAULT_CHARSET,OUT_DEFAULT_PRECIS,CLIP_DEFAULT_PRECIS,DEFAULT_QUALITY,DEFAULT_PITCH | FF_DONTCARE,TEXT("Arial"));
 }
 
@@ -15,144 +14,16 @@ BListCtrl::~BListCtrl(void)
 
 
 BEGIN_MESSAGE_MAP(BListCtrl, CListCtrl)
-	ON_WM_PAINT()
+	//ON_WM_PAINT()
 	ON_WM_DRAWITEM()
 END_MESSAGE_MAP()
 
 
-ControlItem* BListCtrl::GetControlItem(unsigned int line, unsigned int col)
-{
-	for(int i = 0;i< m_controlItems.GetSize();i++)
-	{
-		ControlItem* item = (ControlItem*)m_controlItems.Get(i);
-		if(item->line == line && item->column == col)
-			return item;
-	}
-	return NULL;
-}
-
-bool BListCtrl::GetControlItemData(unsigned int line, unsigned int col, unsigned int* data)
-{
-	ControlItem* listItem = (ControlItem*)GetControlItem(line,col);
-	if(listItem == NULL)
-		return false;
-	*data  = listItem->data;
-	return true;
-}
-
-bool BListCtrl::SetControlItemData(unsigned int line, unsigned int col, unsigned int data)
-{
-	ControlItem* listItem = (ControlItem*)GetControlItem(line,col);
-	if(listItem == NULL)
-		return false;
-	listItem->data = data;
-	this->RedrawItems(line,line);
-	return true;
-}
-
-bool BListCtrl::HasControlItem(unsigned int line)
-{
-	for(int i = 0;i< m_controlItems.GetSize();i++)
-	{
-		ControlItem* item = (ControlItem*)m_controlItems.Get(i);
-		if(item->line == line)
-			return true;
-	}
-	return false;
-}
 
 
 int BListCtrl::GetColumnsCount()  {return GetHeaderCtrl()->GetItemCount();}
 
-bool BListCtrl::AddControlToItem(int i,int j,unsigned char controlType, unsigned int data)
-{
-	ControlItem item;
 
-	if( i< 0 || i>= GetItemCount())
-		return false;
-	item.line = i;
-	item.column =j;
-	item.type = controlType;
-	item.wnd = NULL;
-	item.data = data;
-	m_controlItems.Push(&item);
-	return true;
-
-}
-void BListCtrl::OnPaint()
-{
-	// TODO: Add your message handler code here
-	// Do not call CListCtrl::OnPaint() for painting messages
-
-	int percent;
-	int Top=GetTopIndex();
-	int Total=GetItemCount();
-	int PerPage=GetCountPerPage();
-	int LastItem=((Top+PerPage)>Total)?Total:Top+PerPage;
-
-	// if the count in the list os not zero delete all the progress controls and them procede
-	{
-		int Count=(int)m_controlItems.GetSize();
-		for(int i=0;i<Count;i++)
-		{
-			ControlItem* controlItem=(ControlItem*)m_controlItems.Get(i);
-			if(IsWindow(controlItem->wnd->GetSafeHwnd()))
-				controlItem->wnd->DestroyWindow();
-			//m_controlItems.RemoveAt(0);
-		}
-	}
-
-	
-	CHeaderCtrl* pHeader=GetHeaderCtrl();
-
-	
-
-	for(int i=Top;i<LastItem;i++)
-	{
-		CRect ColRt;
-		if(HasControlItem(i))
-		{
-			for(int j=0;j<GetColumnsCount();j++)
-			{
-				ControlItem* item = GetControlItem(i,j);
-				if(item != NULL)
-				{
-					pHeader->GetItemRect(j,&ColRt);	
-					// get the rect
-					CRect rt;
-					GetItemRect(i,&rt,LVIR_LABEL);
-					rt.top+=1;
-					rt.bottom-=1;
-					rt.left+=ColRt.left+2;
-					int Width=ColRt.Width();
-					rt.right=rt.left+Width-8;
-					/*
-					rt.left=ColRt.left+1;
-					rt.right=ColRt.right-1;
-					*/
-
-					// create the progress control and set their position
-					if(item->type == TYPE_PROGRESS_BAR)
-					{
-							CProgressCtrl* pControl=new CProgressCtrl();
-							pControl->Create(WS_VISIBLE | WS_CHILD,rt,this,IDC_PROGRESS_LIST+i);
-							percent = item->data;
-							// set the position on the control
-							pControl->SetPos(percent);
-							pControl->ShowWindow(SW_SHOWNORMAL);
-							item->wnd = pControl;
-					}
-				
-				}
-			}
-		}
-		
-		
-
-		
-	}
-	CListCtrl::OnPaint();
-}
 
 
 void BListCtrl::DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct)
@@ -162,85 +33,95 @@ void BListCtrl::DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct)
 	CDC* dc;
 	CString text;
 	CRect ColRt;
+	DWORD itemData;
+	unsigned char controlType;
+	unsigned char itemStatus;
 
 
 	rItem = lpDrawItemStruct->rcItem;
 	dc = CDC::FromHandle(lpDrawItemStruct->hDC);
-	//GetText(lpDrawItemStruct->itemID, text);
-	
-
-
-	
-	// If CListBox item selected, draw the highlight rectangle.
-	// Or if CListBox item deselected, draw the rectangle using the window color.
-	if ((lpDrawItemStruct->itemState & ODS_SELECTED) && (lpDrawItemStruct->itemAction & (ODA_SELECT | ODA_DRAWENTIRE)))
+		
+	if(GetItemState(lpDrawItemStruct->itemID, LVIS_SELECTED)& LVIS_SELECTED)
 	{
 		CBrush br(::GetSysColor(COLOR_HIGHLIGHT));
 		dc->FillRect(&rItem, &br);
+
 	}
-	else if (!(lpDrawItemStruct->itemState & ODS_SELECTED) && (lpDrawItemStruct->itemAction & ODA_SELECT)) 
+	else
 	{
 		CBrush br(::GetSysColor(COLOR_WINDOW));
 		dc->FillRect(&rItem, &br);
 	}
 
-	// If the CListBox item has focus, draw the focus rect.
-	// If the item does not have focus, erase the focus rect.
-	if ((lpDrawItemStruct->itemAction & ODA_FOCUS) && (lpDrawItemStruct->itemState & ODS_FOCUS))
-	{
-		dc->DrawFocusRect(&rItem); 
-	}
-	else if ((lpDrawItemStruct->itemAction & ODA_FOCUS) && !(lpDrawItemStruct->itemState & ODS_FOCUS))
-	{
-		dc->DrawFocusRect(&rItem); 
-	}
-
-	
-
-	int iBkMode = dc->SetBkMode(TRANSPARENT);
-	if(GetItemText(lpDrawItemStruct->itemID,0).Compare("Error")==0)
-	{
-		crText = RGB(255,0,0);
-	}
-	else
-		crText = COLOR_3DFACE;
-	
-
+	int iBkMode = dc->SetBkMode(TRANSPARENT);	
 	//Draw the Text
 	CHeaderCtrl* pHeader=GetHeaderCtrl();
 
+	itemData = GetItemData(lpDrawItemStruct->itemID);
+	crText = RGB(0,0,0);
+	if(itemData != 0)
+	{
+		if(GET_ITEM_TYPE(itemData) == ITEM_TYPE_ERROR)
+			crText = RGB(255,0,0);
+	}
+
 	for(int i=0;i<GetColumnsCount();i++)
 	{
-		pHeader->GetItemRect(i,&ColRt);		
-		text = this->GetItemText(lpDrawItemStruct->itemID,i);
-		if(text.GetLength() > 0)
+		pHeader->GetItemRect(i,&ColRt);			
+		ColRt.left+=rItem.left;
+		ColRt.left+=5;
+		ColRt.right+=rItem.left;
+		ColRt.right-=5;
+		ColRt.top = lpDrawItemStruct->rcItem.top;
+		ColRt.bottom = lpDrawItemStruct->rcItem.bottom;
+		if( i == 0 || GET_ITEM_CONTROLTYPE(itemData) == 0)
 		{
-			ColRt.left+=5;
-			ColRt.right-=5;
-			ColRt.top = lpDrawItemStruct->rcItem.top;
-			ColRt.bottom = lpDrawItemStruct->rcItem.bottom;
-			dc->SetTextColor(crText);
-			dc->SelectObject(&simpleFont);
-			dc->DrawText(text,ColRt,DT_LEFT);
-			//dc->TextOut(ColRt.left+5,rItem.top,text.GetString());	
+			text = this->GetItemText(lpDrawItemStruct->itemID,i);	
+			if(text.GetLength() > 0)
+			{
+				
+				dc->SetTextColor(crText);
+				dc->SelectObject(&simpleFont);
+				dc->DrawText(text,ColRt,DT_LEFT);
+				
+			}
 		}
+		else
+		{
+			controlType = GET_ITEM_CONTROLTYPE(itemData);
+			itemStatus = GET_ITEM_STATUS(itemData);
+			if(controlType == ITEM_CONTROLTYPE_PROGRESS_BAR)
+			{
+				double length;
+				
+				dc->DrawFocusRect(ColRt);
+				ColRt.top+=1;
+				ColRt.bottom-=1;
+				length = ColRt.Width() * ((double)itemStatus/100);
+				ColRt.right = ColRt.left + length;
+				CBrush br(::GetSysColor(COLOR_HIGHLIGHT));
+				dc->FillRect(ColRt,&br);
+			}
+		}
+		
+		
+		
 	}
 	
 
 }
-/*
-void BListCtrl::OnDrawItem(NMHDR* pNMHDR, LRESULT* pResult )
+
+bool BListCtrl::SetProgress(int itemId, int progress)
 {
-	CString text;
-	LPNMLVCUSTOMDRAW lplvcd = (LPNMLVCUSTOMDRAW)pNMHDR;
-
-	int nRow = lplvcd->nmcd.dwItemSpec; // get row number
-	text = GetItemText(nRow,0);
-	if(text.CompareNoCase("[BitConnector] ")==0)
-	{
-		lplvcd->clrText = RGB(255,0,0);
-	}
-
-	*pResult=CDRF_NEWFONT|CDRF_NOTIFYSUBITEMDRAW;
+	DWORD itemData;
+	if(itemId<0 || itemId>=GetItemCount())
+		return false;
+	if(progress <0 || progress > 100)
+		return false;
+	itemData = GetItemData(itemId);
+	if(itemData == 0)
+		return false;
+	if(GET_ITEM_CONTROLTYPE(itemData)!= ITEM_CONTROLTYPE_PROGRESS_BAR)
+		return false;
+	return SetItemData(itemId,CREATE_ITEM_STATUS(GET_ITEM_TYPE(itemData),progress,GET_ITEM_CONTROLTYPE(itemData)));
 }
-*/
