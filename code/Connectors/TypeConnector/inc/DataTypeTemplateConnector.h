18b10abb18dd72734c31a8c12ac8673d22925ff6
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-07-07 09:27:19 +0000

    Fixing all conectors

--- code/Connectors/TypeConnector/inc/DataTypeTemplateConnector.h
+++ code/Connectors/TypeConnector/inc/DataTypeTemplateConnector.h
@@ -85,6 +85,8 @@ template <class DataType> bool	DataTypeTemplateConnector<DataType>::OnInitConnec
 
 	columns.nrFeatures = conector->GetFeatureCount();
 	nrRecords = conector->GetRecordCount();
+	if (UpdateFeaturesNameFromConnector()==false)
+		return false;
 	if (nrRecords==0) 
 	{
 		notifier->Error("[%s] -> I received 0 records from the parent connector",ObjectName);
