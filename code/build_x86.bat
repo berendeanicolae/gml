5bf838191e737e508b499e5d9dc2ff3cfa4bc591
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-07-10 23:21:20 +0000

    full and single build scrips

--- code/build_x86.bat
+++ code/build_x86.bat
@@ -1,7 +1,8 @@
-	@IF "%1"=="" GOTO HELP
-	@call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-	@python gml_builder.py win32 %*
+@call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+@IF "%1"=="" GOTO FULL_COMPILE
+	@python gml_builder.py -platform:win32 -out:../Bin/ -plugin:%1
 	@GOTO EXIT
-:HELP
-	@echo "Use: %0 <output-folder>"
+:FULL_COMPILE
+	@python gml_builder.py -platform:win32 -out:../Bin/ -full
+	@GOTO EXIT	
 :EXIT
