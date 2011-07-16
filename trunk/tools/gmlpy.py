e5b74f39e748ba62d994f65e8d45e0bdd5452c98
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-07-16 06:00:37 +0000

    gmlpy.py - format nou

--- trunk/tools/gmlpy.py
+++ trunk/tools/gmlpy.py
@@ -8,168 +8,116 @@ def __GetVarType(var):
 	s = str(type(var))
 	s = s.split("'",1)[1].rsplit("'",1)[0]
 	return s
-
 def __GenerateRandomFileName():
 	name = "GML_TEMPLATE_"+str(hex(os.getpid()))+"_"+str(hex(int(time.time())))+"_"+str(hex(int(random.random()*100000000)))+".ini"
 	return name.replace("0x","")
 
 
-def __DictToString(dictionar):
+def __TypeToString(var,level):
+	tip = __GetVarType(var)
+	if tip=='str':
+		if "\"" in var:
+			return "'"+var+"'"
+		return "\""+var+"\""
+	if tip=='int':
+		return str(var);
+	if tip=='float':
+		return str(var);
+	if tip=='bool':
+		return str(var);
+	if tip=='dict':
+		return __DictToString(var,level)
+	if tip=='list':
+		return __ListToString(var,level)
+	return None
+def __DictToString(dictionar,level):
+	add = ""
 	s = ""
-	obj_name = ""
+	if level>0:
+		add = "\t"*(level+1)
+		s = "{\n"
 	for k in dictionar:
 		if __GetVarType(k) != 'str':
 			print("[Error] Key should be a string value "+str(k))
 			return None
-		if k.lower()=="type":
-			obj_name=str(dictionar[k])
-			continue
-		s += (k+"=")
-		v = dictionar[k]
-		tip = __GetVarType(v)
-		if tip == 'str':
-			s+=(v+";")
-		elif tip == 'int':
-			s+=(str(v)+";")
-		elif tip == 'float':
-			s+=(str(v)+";")
-		elif tip == 'bool':
-			s+=(str(v)+";")
-		else:
-			print("[Error] Unknwon type for "+k+" => "+tip)
-			return None
-	if len(obj_name)==0:
-		print("[Error] Missing 'type' property for "+str(dictionar))
-		return NULL
-	return obj_name+"{"+s+"}"
-def __ListOfDictToString(lista):
-	s = ""
-	for item in lista:
-		if __GetVarType(item) != 'dict':
-			print("[Error] List should contains only dictionaries "+str(lista))
-			return None
-		r = __DictToString(item)
-		if r==None:
-			return None
-		s += r+"=>"
-	if len(s)<2:
-		print("[Error] Invalid list: "+str(lista)+" => "+s)
-		return None
-	s = s[:-2]
-	return s	
-def __ListOfStringsToString(lista):
-	s = ""
-	for item in lista:
-		if __GetVarType(item) != 'str':
-			print("[Error] List should contains only strings "+str(lista))
-			return None
-		s += item+";"
-	if len(s)==0:
-		print("[Error] Invalid list: "+str(lista)+" => "+s)
-		return None
-	s = s[:-1]
-	return s	
-def __ListOfNumbersToString(lista):
-	s = ""
-	vname = __GetVarType(lista[0])
-	if (vname=='bool'):
-		s = "(BOOL)["
-	elif (vname=='float'):
-		s = "(DOUBLE)["
-	elif (vname=='int'):
-		s = "(INT32)["
-	else:
-		s = "["
-	for item in lista:
-		s+=str(item)+","
-	if len(s)==0:
-		print("[Error] Invalid list: "+str(lista)+" => "+s)
-		return None
-	s = s[:-1]+"]"
-	return s	
-def __ListToString(lista):
+		s+=add+k+" = "
+		res = __TypeToString(dictionar[k],level+1)		
+		if (res==None):
+			print("[Error] Invalid value for key: "+str(k))
+			return None	
+		s+=res+"\n"
+	if level>0:
+		s+=add[1:]+"}"
+	return s
+def __ListToString(lista,level):
+	# analizez datele inainte
 	if len(lista)==0:
-		return "\"\""
-	typeName = __GetVarType(lista[0])
-	for item in lista:
-		vname = __GetVarType(item)
-		if (vname!=typeName):
-			print("[Error] Invalid element in list ("+str(item)+") of type "+vname+" -> should be of type : "+typeName)
-			return None
-	if typeName == 'dict':
-		return "\""+__ListOfDictToString(lista)+"\""
-	elif typeName == 'str':
-		return "\""+__ListOfStringsToString(lista)+"\""
-	elif typeName == 'int':
-		return __ListOfNumbersToString(lista)
-	elif typeName == 'float':
-		return __ListOfNumbersToString(lista)
-	elif typeName == 'bool':
-		return __ListOfNumbersToString(lista)
-	else:
-		print("[Error] Lists should contain dict elements or string elements only")
+		print("[Error] Lists should not be empty ")
 		return None	
-def CreateTemplateFile(dictionar,fname,checkForAlgorithm = False):
-	s = ""
-	alg = False
-	cmd = False
-	for k in dictionar:
-		if __GetVarType(k) != 'str':
-			print("[Error] Key should be a string value "+str(k))
-			return False
-		if k.lower()=='algorithmname':
-			alg = True
-		if k.lower()=='command':
-			cmd = True
-		s += (k+" = ")
-		v = dictionar[k]
-		tip = __GetVarType(v)
-		if tip == 'str':
-			s+=("'"+v+"'\n")
-		elif tip == 'int':
-			s+=(str(v)+"\n")
-		elif tip == 'float':
-			s+=(str(v)+"\n")
-		elif tip == 'bool':
-			s+=(str(v)+"\n")
-		elif tip == 'dict':
-			r = __DictToString(v)
-			if r==None:
-				return False
-			s+="\""+r+"\"\n"
-		elif tip == 'list':
-			r = __ListToString(v)
-			if r==None:
-				return False
-			s+=r+"\n"
+	tip = __GetVarType(lista[0])
+	for i in lista:
+		if __GetVarType(i)!=tip:
+			print("[ERROR] Invalid tip type of data in list : "+str(i)+" -> should be "+tip)
+			return None
+	#creez lista in functie de tip
+	add = ""
+	if level>0:
+		add = "\t"*level;
+	sep = ";\n"
+	final = add+"]\n"
+	s = "[\n"
+	add = "\t"*(level+1)
+	if tip=='bool':
+		s = "(BOOL) ["
+		sep = ","
+		final = "]"
+		add = ""
+	if tip=='int':
+		s = "(INT32) ["
+		sep = ","
+		final="]"
+		add = ""
+	if tip=='float':
+		s = "(DOUBLE) ["
+		sep = ","
+		final="]"
+		add = ""
+	index = 0
+	for element in lista:
+		res = __TypeToString(element,level+1)
+		if (res==None):
+			print("[Error] Invalid value for element: "+str(element))
+			return None	
+		s+=add+res
+		index+=1
+		if sep==",":
+			if index<len(lista):
+				s+=sep
 		else:
-			print("[Error] Unknwon type for "+k+" => "+tip)
-			return False
-	if checkForAlgorithm:
-		if alg==False:
-			print("[Error] Missing 'Algorithm' property")
-			return False
-		if cmd==False:
-			print("[Error] Missing 'Command' property")
-			return False
-	
+			s+=sep
+	s+=final
+	return s	
+def CreateTemplateFile(dictionar,fname):
+	s = __DictToString(dictionar,0)
+	if s==None:
+		print("[ERROR] Invalid format ...")
+		return False
 	try:
 		f = open(fname,"w")
 		f.write(s)
 		f.close()
 		return True
 	except:
-		print("[Error] Unable to create/write: "+fname)
-	
+		print("[Error] Unable to create/write: "+fname)	
 	return False
 			
 def Run(dictionar):
 	fname = __GenerateRandomFileName()
 	if __GetVarType(dictionar)=="instance":
 		dictionar = dictionar._attr;
-	if CreateTemplateFile(dictionar,fname,True)==False:
+	if CreateTemplateFile(dictionar,fname)==False:
 		return False
-	os.system("\""+os.path.join(GML_PATH,"gml.exe")+"\" run \""+fname+"\"")
+	os.system(os.path.join(GML_PATH,"gml.exe")+" run \""+fname+"\"")
 	try:
 		os.unlink(fname)
 	except:
