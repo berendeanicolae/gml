49a65b7a7c7fd526c78fc2cfad32a0050947c906
gavrilut.dragos <gavrilut.dragos@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2011-07-17 13:13:20 +0000

    IndexBitConnector fixup

--- code/Connectors/IndexBitConnector/src/IndexBitConnector.cpp
+++ code/Connectors/IndexBitConnector/src/IndexBitConnector.cpp
@@ -247,43 +247,13 @@ bool	IndexBitConnector::OnInitConnectionToDataBase()
 	double										cValue;
 	IndexBitCounter								ibc;
 
-	if (database->Connect()==false)
-	{
-		notifier->Error("[%s] -> Could not connect to database",ObjectName);
-		return false;
-	}
-	tempStr.SetFormated("%s LIMIT 1",Query.GetText());
-	if (UpdateColumnInformations(tempStr.GetText())==false)
-		return false;
-	if (QueryRecordsCount(CountQuery.GetText(),nrRecords)==false)
-		return false;
-	if (nrRecords==0) 
-	{
-		notifier->Error("[%s] -> I received 0 records from the database",ObjectName);
-		return false;
-	}
 	memset(&ibc,0,sizeof(ibc));
 	notifier->StartProcent("[%s] -> Analizing DataBase : ",ObjectName);
 	for (tr=0;tr<nrRecords;tr++)
 	{
-		// cache
-		if ((tr % CachedRecords)==0)
+		if (database->ReadNextRecord(VectPtr)==false)
 		{
-			if (tr+CachedRecords<nrRecords)
-				tempStr.SetFormated("%s LIMIT %d,%d",Query.GetText(),tr,CachedRecords);
-			else
-				tempStr.SetFormated("%s LIMIT %d,%d",Query.GetText(),tr,nrRecords-tr);
-			//notifier->Info("%s",tempStr.GetText());
-			if (database->ExecuteQuery(tempStr.GetText())==false)
-			{
-				notifier->Error("[%s] -> Unable to Execute query : %s !",ObjectName,tempStr.GetText());
-				return false;
-			}
-			notifier->SetProcent((double)tr,(double)nrRecords);
-		}
-		if (database->FetchNextRow(VectPtr)==false)
-		{
-			notifier->Error("[%s] -> Error reading #%d record !",ObjectName,tr);
+			notifier->Error("[%s] -> Unable to read #d record from database!",ObjectName,tr);
 			return false;
 		}
 		// pentru fiecare record pun valorile
@@ -294,12 +264,18 @@ bool	IndexBitConnector::OnInitConnectionToDataBase()
 			if (cValue!=0.0)
 				Update(ibc,gr);				
 		}
+		if ((tr % 10000)==0)
+			notifier->SetProcent(tr,nrRecords);
 	}	
 	notifier->EndProcent();
 	ComputeMemory(ibc,MemToAlloc);
 	if (AllocMemory(MemToAlloc)==false)
 		return false;
-	// sunt exact la inceput
+	if (database->BeginIteration()==false)
+	{
+		notifier->Error("[%s] -> Error on DataBase::BeginIteration()",ObjectName);
+		return false;
+	}
 	
 	cIndex = 0;
 	notifier->StartProcent("[%s] -> Loading DataBase : ",ObjectName);
@@ -307,23 +283,9 @@ bool	IndexBitConnector::OnInitConnectionToDataBase()
 	for (tr=0;tr<nrRecords;tr++)
 	{
 		Indexes[tr] = (UInt32)cIndex;
-		if ((tr % CachedRecords)==0)
+		if (database->ReadNextRecord(VectPtr)==false)
 		{
-			if (tr+CachedRecords<nrRecords)
-				tempStr.SetFormated("%s LIMIT %d,%d",Query.GetText(),tr,CachedRecords);
-			else
-				tempStr.SetFormated("%s LIMIT %d,%d",Query.GetText(),tr,nrRecords-tr);
-			//notifier->Info("%s",tempStr.GetText());
-			if (database->ExecuteQuery(tempStr.GetText())==false)
-			{
-				notifier->Error("[%s] -> Unable to Execute query : %s !",ObjectName,tempStr.GetText());
-				return false;
-			}
-			notifier->SetProcent((double)tr,(double)nrRecords);
-		}
-		if (database->FetchNextRow(VectPtr)==false)
-		{
-			notifier->Error("[%s] -> Error reading #%d record !",ObjectName,tr);
+			notifier->Error("[%s] -> Unable to read #d record from database!",ObjectName,tr);
 			return false;
 		}
 		// pentru fiecare record pun valorile
@@ -353,6 +315,8 @@ bool	IndexBitConnector::OnInitConnectionToDataBase()
 			}
 		}
 		// trecem la urmatorul record		
+		if ((tr % 10000)==0)
+			notifier->SetProcent(tr,nrRecords);
 	}	
 	notifier->EndProcent();
 	if (cIndex!=MemToAlloc)
