cb83edb01e105a3e13a9864025e4d377cc9a4a4e
mihai@imcu.ro <mihai@imcu.ro@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2012-02-01 07:49:26 +0000

    adding debug build scripts

---
+++ trunk/code/build_x86_debug.bat
@@ -0,0 +1,8 @@
+@call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+@IF "%1"=="" GOTO FULL_COMPILE
+	@python gml_builder_debug.py -platform:win32 -out:../Bin/ -plugin:%1
+	@GOTO EXIT
+:FULL_COMPILE
+	@python gml_builder_debug.py -platform:win32 -out:../Bin/ -full
+	@GOTO EXIT	
+:EXIT
