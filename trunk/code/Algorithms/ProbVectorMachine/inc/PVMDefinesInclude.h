6e346dea0aa6dde6cb3c466c29a25d1f6b514ef5
mihai@imcu.ro <mihai@imcu.ro@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2012-02-04 15:44:04 +0000

    adding macros in file PVMMacros.h

--- trunk/code/Algorithms/ProbVectorMachine/inc/PVMDefinesInclude.h
+++ trunk/code/Algorithms/ProbVectorMachine/inc/PVMDefinesInclude.h
@@ -2,5 +2,8 @@
 #define __PVM_DEFINES_INCLUDE_H__
 //-----------------------------------------------------------------------------
 #include "PVMDefinesTypes.h"
+#include "PVMMacros.h"
 //-----------------------------------------------------------------------------
+#define  UNGIGA (1024*1024*1024)
+
 #endif//__PVM_DEFINES_INCLUDE_H__
