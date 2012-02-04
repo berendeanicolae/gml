7edf5618db82d4592d011b2ad378c09b24ecf5d9
mihai@imcu.ro <mihai@imcu.ro@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2012-02-04 16:32:00 +0000

    pvm debugging

--- trunk/code/Algorithms/ProbVectorMachine/inc/PVMMacros.h
+++ trunk/code/Algorithms/ProbVectorMachine/inc/PVMMacros.h
@@ -4,20 +4,43 @@
 #include <cstring>
 #include <cstdlib>
 
+#define PVM_DEBUG_MODE 1
+
 #define INFOMSG(...) { {char z123[4096]; sprintf_s(z123,4096, __VA_ARGS__) ;notif->Info("[%s] -> %s",ObjectName, z123);} };
 #define INFOTHDMSG(...) { {char z123[4096]; sprintf_s(z123,4096, __VA_ARGS__) ;notif->Info("[%s] [Th:%d] -> %s",ObjectName, thData.ThreadID, z123);} };
 #define ERRORMSG(...){ {char z123[4096]; sprintf_s(z123,4096, __VA_ARGS__) ;notif->Error("[%s] -> %s",ObjectName, z123);} };
 
 #define NULLCHECKMSG(val, ...){ if (val==NULL) { ERRORMSG(__VA_ARGS__); return false; } };    
-
 #define NULLCHECK(val) {if (val==NULL) { return false; }; };
 
 #define CHECKMSG(val, ...){ if (val==false) { ERRORMSG(__VA_ARGS__); return false; }; };
 #define CHECK(val){ if (val==false) return false;}; 
 
-#define CLCHECK(val) { if (val!=CL_SUCCESS) return false;}
-#define CLDISPLAYERR { notif->Error("[%s] -> Line: %d; OpenCL Error: %s", ObjectName, __LINE__, GetCLErrorMsg(ErrorCode));}
-#define CLCHECKMSG(...) { if (ErrorCode!=CL_SUCCESS){ ERRORMSG(__VA_ARGS__); CLDISPLAYERR(ErrorCode); return false;}}
+#ifdef PVM_DEBUG_MODE
 
+#define DBG_NULLCHECKMSG    NULLCHECKMSG
+#define DBG_NULLCHECK       NULLCHECK
+#define DBG_CHECKMSG        CHECKMSG
+#define DBG_CHECK           CHECK
+
+#define DBGSTOP_NULLCHECKMSG(val,...)    { if (val==NULL) {ERRORMSG(__VA_ARGS__); DebugBreak();} }
+#define DBGSTOP_NULLCHECK(val,...)       { if (val==NULL) DebugBreak(); } 
+#define DBGSTOP_CHECKMSG(val,...)        { if (val==TRUE) {ERRORMSG(__VA_ARGS__); DebugBreak();} }
+#define DBGSTOP_CHECK(val,...)           { if (val==TRUE) DebugBreak(); } 
+
+#else
+
+#define DBG_NULLCHECKMSG    //
+#define DBG_NULLCHECK       //
+#define DBG_CHECKMSG        //
+#define DBG_CHECK           //
+
+#define DBGSTOP_NULLCHECKMSG    //
+#define DBGSTOP_NULLCHECK       //
+#define DBGSTOP_CHECKMSG        //
+#define DBGSTOP_CHECK           //
+
+
+#endif
 
 #endif //__PVM_MACROS_H_
