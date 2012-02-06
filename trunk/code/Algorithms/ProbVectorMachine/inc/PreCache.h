358045bd29a52ef5b62c69961f8b2de10cb32bbb
mihai@imcu.ro <mihai@imcu.ro@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2012-02-06 09:59:11 +0000

    forgotten file

---
+++ trunk/code/Algorithms/ProbVectorMachine/inc/PreCache.h
@@ -0,0 +1,72 @@
+#ifndef __PRE_CACHE_H_
+#define __PRE_CACHE_H_
+
+#include "gmllib.h"
+#include "PVMMacros.h"
+#include "PVMDefinesInclude.h"
+#include "PVMDefinesTypes.h"
+
+#include <cstring>
+
+using namespace GML::Utils;
+
+class PreCache : GML::Utils::GMLObject {
+
+public:
+
+	struct InheritData {
+		GML::ML::IConnector*	con;
+		GML::Utils::INotifier*	notif;	
+
+		unsigned int VarKernelType;
+		unsigned int VarPreCacheFileSize;
+		unsigned int VarPreCacheBatchStart;
+		unsigned int VarPreCacheBatchCount;
+
+		GML::Utils::GString	VarPreCacheFilePattern;
+		
+		~InheritData() {}
+	};
+
+private:
+
+	struct Map_PreCacheComputeBatch {
+		UInt32 CurrBatchNr;
+		UInt32 RecStart;
+		UInt32 RecCount;
+		pvm_float* Buffer;
+	};
+	Map_PreCacheComputeBatch pccb;
+
+	// configuration data
+	InheritData id;
+	GML::Algorithm::IMLAlgorithm* alg;
+	GML::ML::IConnector*	con;
+	GML::Utils::INotifier*	notif;
+
+	UInt32 NrRec, NrFts;
+
+	UInt32 TotalNrBatches;
+	UInt32 RecPerBatch;
+
+	UInt32 SizePerLine;
+
+	// internal procs
+	int GetNrRecPerBatch(int MinNr, int MaxNr);
+	int GetSizeOfBatch(int BatchNr);
+	
+public:
+	// constructors
+	PreCache();
+
+	// destructor
+	~PreCache();	
+
+	// main procedure
+	bool SetInheritData(PreCache::InheritData &InhData);
+	bool SetParentAlg(GML::Algorithm::IMLAlgorithm* _alg);
+	bool PreCompute();
+	bool ThreadPrecomputeBatch(GML::Algorithm::MLThreadData &thData);
+};
+
+#endif //__PRE_CACHE_H_
