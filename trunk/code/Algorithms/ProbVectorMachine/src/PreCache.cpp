85c5962e9f4f70f2664db5bf5336c1da517c22de
mihai@imcu.ro <mihai@imcu.ro@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2012-02-19 17:43:36 +0000

    finished kprime computing and precache files fix

--- trunk/code/Algorithms/ProbVectorMachine/src/PreCache.cpp
+++ trunk/code/Algorithms/ProbVectorMachine/src/PreCache.cpp
@@ -17,10 +17,12 @@ bool PreCache::SetInheritData(PreCache::InheritData &InhData)
 
 bool PreCache::PreCompute()
 {
-	GML::Utils::File BatchFileObj;
-	GML::Utils::GString BatchFileName; 
-	unsigned char *Buffer, *WriteBuffer;
-	unsigned long long SzOfBatch, WrittenSoFar, Written, BlockSize;
+	GML::Utils::File KernelFileObj, KPrimeFileObj;
+	GML::Utils::GString KernelFileName, KPrimeFileName; 
+	unsigned char *KernelBuffer, *WriteBuffer;
+	KPrimePair  *KPrimeBuffer;
+	unsigned long long SzOfBatch, WrittenSoFar, Written, BlockSize;	
+	PreCacheFileHeader pcfh;
 
 	NrRec = id.con->GetRecordCount();
 	NrFts = id.con->GetFeatureCount();
@@ -33,18 +35,33 @@ bool PreCache::PreCompute()
 		id.VarPreCacheBatchCount = TotalNrBatches;
 
 	// allocate memory for output buffer
-	Buffer = (unsigned char*) malloc (id.VarPreCacheFileSize*UNMEGA);
-	NULLCHECKMSG(Buffer, "could not allocate enough memory for output buffer");
-	pccb.Buffer = (pvm_float*) Buffer;
+	KernelBuffer = (unsigned char*) malloc (id.VarPreCacheFileSize*UNMEGA);
+	NULLCHECKMSG(KernelBuffer, "could not allocate enough memory for kernel output buffer");
+	pccb.KernelBuffer = (pvm_float*) KernelBuffer;
+	memset(KernelBuffer, 0, id.VarPreCacheFileSize*UNMEGA);
+
+	KPrimeBuffer = (KPrimePair*) malloc (RecPerBatch*sizeof(KPrimePair));
+	NULLCHECKMSG(KPrimeBuffer, "could not allocate enough memory for kprime output buffer");
+	pccb.KPrimeBuffer = KPrimeBuffer;
+	memset(KPrimeBuffer, 0, RecPerBatch*sizeof(KPrimePair));
+
+	// records per batch, useful for computing where we are right now
+	pccb.RecPerBatch = RecPerBatch;
 
 	// iterate through the batches that need to be computed
-	for (UInt32 i=id.VarPreCacheBatchStart;i<id.VarPreCacheBatchStart+id.VarPreCacheBatchCount;i++) {
+	for (UInt32 i=id.VarPreCacheBatchStart;i<id.VarPreCacheBatchStart+id.VarPreCacheBatchCount && i*RecPerBatch<NrRec;i++) {
 		
 		// create the current output file
-		BatchFileName.Truncate(0);
-		BatchFileName.Add(id.VarPreCacheFilePattern);
-		BatchFileName.AddFormated("%03d", i);
-		CHECKMSG(BatchFileObj.Create(BatchFileName), "could not create file: %s", BatchFileName.GetText());
+		KernelFileName.Truncate(0);
+		KernelFileName.Add(id.VarPreCacheFilePrefix);
+		KernelFileName.AddFormated(".kernel.%02d", i);		
+
+		KPrimeFileName.Truncate(0);
+		KPrimeFileName.Add(id.VarPreCacheFilePrefix);
+		KPrimeFileName.AddFormated(".kprime.%02d", i);
+
+		CHECKMSG(KernelFileObj.Create(KernelFileName), "could not create file: %s", KernelFileName.GetText());
+		CHECKMSG(KPrimeFileObj.Create(KPrimeFileName), "could not create file: %s", KPrimeFileName.GetText());
 
 		// set the current batch number for the map operation
 		pccb.CurrBatchNr = i;
@@ -55,25 +72,51 @@ bool PreCache::PreCompute()
 		// run the actual map command on threads
 		alg->ExecuteParalelCommand(ProbVectorMachine::THREAD_COMMAND_PRECOMPUTE_BATCH);
 
+		//
 		// write the out buffer to the disk
+		// 
 		SzOfBatch = GetSizeOfBatch(i);
-		WriteBuffer = Buffer;
+		WriteBuffer = KernelBuffer;
+
+		// set the file header fields
+		memset(pcfh.Magic, 0, 32);		
+		sprintf_s((char*)pcfh.Magic,strlen(PRECACHE_FILE_HEADER_MAGIC)+1,PRECACHE_FILE_HEADER_MAGIC);		
+
+		pcfh.NrFeatures = NrFts;
+		pcfh.NrRecordsTotal = NrRec;
+		pcfh.NrRecords = pccb.RecCount;
+		pcfh.RecordStart = i*RecPerBatch;		
+
+		//write the header of the PreCache file
+		DBG_CHECKMSG(KernelFileObj.Write(0, &pcfh, sizeof(PreCacheFileHeader), &Written), "failed to write kernel values file header");
 
 		// the write loop
-		WrittenSoFar = 0;
-		while (WrittenSoFar < SzOfBatch) {			
+		WrittenSoFar = Written;
+		while (WrittenSoFar < SzOfBatch+sizeof(PreCacheFileHeader)) {			
 			// figure how much to write to this block
-			if (SzOfBatch-WrittenSoFar < UNMEGA) BlockSize = SzOfBatch-WrittenSoFar;
-			else BlockSize = UNMEGA;
+			//if (SzOfBatch-WrittenSoFar < UNMEGA) BlockSize = SzOfBatch-WrittenSoFar;
+			//else BlockSize = UNMEGA;
 		
 			// the write operation
-			DBG_CHECKMSG(BatchFileObj.Write(WrittenSoFar, WriteBuffer, UNMEGA, &Written), "failed to write on disk");
+			DBG_CHECKMSG(KernelFileObj.Write(WrittenSoFar, WriteBuffer, UNMEGA, &Written), "failed to write kernel values buffer");
 			WrittenSoFar += Written;
 		}
-		BatchFileObj.Close();
+
+		memset(pcfh.Magic, 0, 32);
+		sprintf_s((char*)pcfh.Magic,strlen(KPRIME_FILE_HEADER_MAGIC)+1,KPRIME_FILE_HEADER_MAGIC);
+		//write the header of the PreCache file
+		DBG_CHECKMSG(KPrimeFileObj.Write(0, &pcfh, sizeof(PreCacheFileHeader), &Written), "failed to write kprime values file header");
+		// write kprim buffer
+		DBG_CHECKMSG(KPrimeFileObj.Write(Written, KPrimeBuffer, pccb.RecCount*sizeof(KPrimePair), &Written), "failed to write kprime buffer");
+
+		KernelFileObj.Close();
+		KPrimeFileObj.Close();
+		pccb.BatchNr ++;
 	}
-	// free the buffer
-	free(Buffer);
+
+	// free the buffers
+	free(KernelBuffer);
+	free(KPrimeBuffer);
 
 	return true;
 }
@@ -84,6 +127,10 @@ int PreCache::GetNrRecPerBatch(int MinNr, int MaxNr)
 	UInt32 MidNr = MinNr + (MaxNr-MinNr)/2;
 	UInt32 SzForMid = (MidNr*NrRec - (MidNr*(MidNr-1))/2)*sizeof(pvm_float);
 	UInt32 SzPerBatch = id.VarPreCacheFileSize*UNMEGA;
+	UInt32 SzForAll = (NrRec*NrRec - (NrRec*(NrRec-1))/2)*sizeof(pvm_float);
+
+	// what if we the database is smaller than UNMEGA
+	if (SzForAll<SzPerBatch) return MaxNr;
 
 	// the searched element condition
 	if (SzForMid > SzPerBatch-SizePerLine && SzForMid <= SzPerBatch) return MidNr;
@@ -107,7 +154,7 @@ bool PreCache::SetParentAlg(GML::Algorithm::IMLAlgorithm* _alg)
 
 bool PreCache::ThreadPrecomputeBatch(GML::Algorithm::MLThreadData &thData)
 {
-	int ThreadId = thData.ThreadID, OutPos, NrLines;
+	int ThreadId = thData.ThreadID, OutPos, LineNr;
 	GML::ML::MLRecord one, two;
 	pvm_float sum;
 
@@ -116,33 +163,48 @@ bool PreCache::ThreadPrecomputeBatch(GML::Algorithm::MLThreadData &thData)
 
 	if (ThreadId == 0) id.notif->StartProcent("[%s] -> Computing batch number %03d ... ",ObjectName, pccb.CurrBatchNr);	
 
-	ker_f_wrapper kernel(con, notif);
-	// todo: add a check here for return value of function, when it will be BOOL
-	kernel.set_params(id.VarKernelParamDouble, id.VarKernelParamInt, NULL, (KerFuncType)id.VarKernelType);
+	ker_f_wrapper kernel(con, notif);	
+	CHECKMSG(kernel.set_params(id.VarKernelParamDouble, id.VarKernelParamInt, NULL, (KerFuncType)id.VarKernelType),"could not set kernel parameters");
 	
 	// the outer loop where every thread alternated and takes a record
 	for (UInt32 i=pccb.RecStart+ThreadId; i<pccb.RecStart+pccb.RecCount; i+=alg->threadsCount) {
 
-		NrLines = i-pccb.RecStart;
+		LineNr = i-pccb.RecStart;
 
 		// get ml record from connector
 		DBG_CHECKMSG(id.con->GetRecord(one, i),"could not get ml record from connector");
 
 		// iterate through all records
-		for (UInt32 j=NrLines;j<NrRec;j++) {
+		for (UInt32 j=LineNr;j<NrRec;j++) {
 
 			// get second ml record here
 			DBG_CHECKMSG(id.con->GetRecord(two, j),"could not get ml record from connector");
 			
-			// insert kernel here; placeholder now
+			// the kernel computation
 			sum = (pvm_float)kernel.compute_for(one, two);
 
 			// put the kernel value to its out place			
-			OutPos = (NrLines*NrRec - NrLines*(NrLines-1)/2) + j-NrLines;
-			pccb.Buffer[OutPos] = sum;			
+			OutPos = (LineNr*NrRec - LineNr*(LineNr-1)/2) + j-LineNr;
+			pccb.KernelBuffer[OutPos] = sum;			
 		}
-		if (ThreadId == 0 && i%10==0) id.notif->SetProcent(i-pccb.RecStart, pccb.RecCount);
+
+		// compute the kprime values for this batch			
+		pvm_float KpPos = 0, KpNeg = 0, val;
+
+		double label;
+		for (UInt32 j=0;j<NrRec;j++) {
+			CHECK(GetKernelAt(LineNr,j, pccb.KernelBuffer, pccb.RecCount, &val));		
+			CHECKMSG(id.con->GetRecordLabel(label, j),"could not record label for index %d", j);		
+			if (label==1) KpPos += val;						
+			else KpNeg += val;		
+		}
+		pccb.KPrimeBuffer[LineNr].pos = KpPos;
+		pccb.KPrimeBuffer[LineNr].neg = KpNeg;
+
+		if (ThreadId == 0 && (i-pccb.RecStart)%10==0) id.notif->SetProcent(i-pccb.RecStart, pccb.RecCount);
 	}
+
+	// end the connector procent progress
 	id.notif->EndProcent();
 
 	return true;
@@ -157,4 +219,15 @@ int PreCache::GetSizeOfBatch(int BatchNr)
 		RecHere = RecPerBatch;
 
 	return (RecHere*NrRec - (RecHere*(RecHere-1))/2)*sizeof(pvm_float);
+}
+
+bool PreCache::GetKernelAt(int line, int row, pvm_float* KernelStorage, int NrRecInBatch, pvm_float *KVal)
+{
+	DBGSTOP_CHECKMSG(line<NrRecInBatch, "GetKernelAt line nr out of bounds");
+	DBGSTOP_CHECKMSG(row<NrRec,"GetKernelAt row out of bounds");
+	if (line>row) { int aux = line; line = row; row=aux; }
+	
+	int OutPos = (line*NrRec - line*(line-1)/2) + row-line;
+	*KVal = KernelStorage[OutPos];
+	return true;
 }
