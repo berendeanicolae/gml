f0ec526e88915a5aab9cb0f5ca4d41d7627984bf
mihai <mihai@imcu.ro>
2012-06-13 19:46:26 +0000

    added 5fold cross validation to master.py and added foldsplit.py to split a database in folds

---
+++ code/Algorithms/ProbVectorMachine/tools/foldsplit.py
@@ -0,0 +1,87 @@
+import gmlpy, sys
+
+def generateDbFraction(dbInput, dbOutput, procStart, procCount):	
+	d = {
+	    "AlgorithmName":"CacheBuilder",
+	    "CacheName":dbOutput,
+	    "Command":"CreateCache",
+	    "Connector":{
+	        "Type":"TypeFloatConnector",
+	        "StoreFeatureName":False,
+	        "StoreRecordHash":False,          		        
+	        "Connector": {        
+	            "Type":"SplitConnector",
+	            "StoreFeatureName":False,
+	            "StoreRecordHash":False,
+	            "SplitMode":"UniformPercentage",
+	            "Start":procStart,
+	            "End":(procStart+procCount)%100,        
+	            "Connector" : {
+	                "Type": "TypeFloatConnector",                
+	                "DataFileName": dbInput,
+	                "StoreFeatureName":False,
+	                "StoreRecordHash":False,
+	            },        
+	        },	    
+	    },
+	    "Notifier":{
+	        "Type":"FileNotifier",
+	        "FileName": "notifier.log",
+	        "UseColors":False
+	    }		
+	}	
+	gmlpy.Run(d)
+	
+def shuffleDb(dbInput, dbOutput):
+	d = {
+	    "AlgorithmName":"CacheBuilder",
+	    "CacheName":dbOutput,
+	    "Command":"CreateCache",
+	    "Connector":{
+	        "Type":"TypeFloatConnector",
+	        "StoreFeatureName":False,
+	        "StoreRecordHash":False,          		        
+	        "Connector" : {	    
+	            "Type" : "ShuffleConnector",
+	            "Method" : "Random",	        
+	            "StoreFeatureName":False,
+	            "StoreRecordHash":False,    	    	            		            	        
+	            "Connector" : {
+	                "Type": "FullCacheConnector",                
+	                "DataFileName": dbInput,
+	                "StoreFeatureName":False,
+	                "StoreRecordHash":False,
+	            },        
+	        },
+	    },
+	    "Notifier":{
+	        "Type":"FileNotifier",
+	        "FileName": "notifier.log",
+	        "UseColors":False
+	    }		
+	}	
+	gmlpy.Run(d)	
+	
+def splitDatabaseInFolds (database, nrOfFolds):	
+
+	shuffledDatabase = database + ".shuffle"
+	shuffleDb(database, shuffledDatabase)
+	
+	splitSize = 100/nrOfFolds
+	for x in range(0,nrOfFolds):
+		procStart1 = splitSize*x
+		procCount1 = splitSize
+		procStart2 = (splitSize + procStart1)%100
+		procCount2 = 100-splitSize
+		generateDbFraction(shuffledDatabase, "%s-%dof%d-test.cache"%(database, x+1, nrOfFolds), procStart1, procCount1)
+		generateDbFraction(shuffledDatabase, "%s-%dof%d-train.cache"%(database, x+1, nrOfFolds), procStart2, procCount2)
+		
+		
+def main ():
+	if len(sys.argv) < 2:
+		print ("usage: foldsplit.py DatabaseFileName")
+		return
+	database = sys.argv[1]
+	splitDatabaseInFolds(database, 5)
+	
+main()
