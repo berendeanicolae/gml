4216598a58bc9cca085809299f7eb841166c8298
mr.andrei.sucila@gmail.com <mr.andrei.sucila@gmail.com@dbee95a5-fea3-17b5-19aa-aea05da7b0b2>
2012-06-21 19:44:40 +0000

    Added the possibility to do complete training and testing for the pvm-script.py. I've also set global variables for the train and test databases and the connector type.

--- trunk/code/Algorithms/ProbVectorMachine/tools/pvm-script.py
+++ trunk/code/Algorithms/ProbVectorMachine/tools/pvm-script.py
@@ -1,36 +1,40 @@
 import os, gmlpy, math, glob, shutil
-
+global trainDataSet, testDataSet
+global connectorType
 def getParameterConfig():
     param = {
         "AlgorithmName" : "ProbVectorMachine",
-        "KernelType" : "Rbf",
-        "KernelParamInt" : 0,
-        "KernelParamDouble" : 0.125,
+        "KernelType" : "Scalar",
+        "KernelParamInt" : 3,
+        "KernelParamDouble" : 0.0,
         
-        "BlockFileSize" : 1,
+        "BlockFileSize" : 1024,
         "BlockStart" : 0,
-        "BlockCount" : 5,
-        "BlockFilePrefix" : "bitdefender",
+        "BlockCount" : 1,
+        "BlockFilePrefix" : "pvm",
         
         "ThreadsCount" : 2,
         
         #training related vars
-        "Lambda" : 1,
-        "WindowSize" : 4,       
-        }
+        "Lambda" : 1.4,
+        "WindowSize" : 2, 
+        "NrUpdatesPerNormBlock" : 20,
+        "NrUpdatesPerLastBlock" : 5,
+        "T" : 1.0
+        } 
     return param
 
 def getConnectorDict ():
     con = {
-        "DataFileName":"500.050P001P.compressed.cache.5",
-        "Type":"BitConnector"
+        "DataFileName":trainDataSet,
+        "Type":connectorType
         }
     return con
 
 def getConnectorTestDict ():
     con = {
-        "DataFileName":"500.050P001P.compressed.cache.test",
-        "Type":"BitConnector"
+        "DataFileName":testDataSet,
+        "Type":connectorType
         }
     return con
 
@@ -95,22 +99,28 @@ def IterateTraining (nrIter, varT, nrIterStable):
     score_vect = []
 
     print ("performing training for T = %.05f"%varT)
+    retValue = False
+    lastIt = 0
     
     for it in range (1, nrIter):
         PerformTrain4Iter(it, varT)    
         ExecuteCommand("GatherBlockStates",it)
+        lastIt = it
         
-        score = open("%s.state.iter.%03d.block.score"%(paramDict["BlockFilePrefix"],it)).read().strip()
+        obj = open("%s.state.iter.%04d.block.score"%(paramDict["BlockFilePrefix"],it))
+        score = obj.read().strip()
+        obj.close()
         score = float(score)
         score_vect.append(score)
         
-        shutil.copy("%s.state.iter.%03d.block.score"%(paramDict["BlockFilePrefix"],it), "%s.state.score.current"%(paramDict["BlockFilePrefix"]))
+        shutil.copy("%s.state.iter.%04d.block.score"%(paramDict["BlockFilePrefix"],it), "%s.state.score.current"%(paramDict["BlockFilePrefix"]))        
         
         # remove garbage
         if it>1:
-            files2rem = glob.glob("%s.state.iter.%03d.block.???"%(paramDict["BlockFilePrefix"],it-1))
-            files2rem += glob.glob("%s.state.iter.%03d.block.last"%(paramDict["BlockFilePrefix"],it-1))
-            files2rem += glob.glob("%s.state.iter.%03d.block.score"%(paramDict["BlockFilePrefix"],it-1))
+            files2rem = glob.glob("%s.state.iter.%04d.block.????"%(paramDict["BlockFilePrefix"],it-1))
+            files2rem += glob.glob("%s.state.iter.%04d.block.last"%(paramDict["BlockFilePrefix"],it-1))
+            files2rem += glob.glob("%s.state.iter.%04d.block.score"%(paramDict["BlockFilePrefix"],it-1))
+            files2rem += glob.glob("%s.state.iter.%04d.block.all"%(paramDict["BlockFilePrefix"],it-1))
             for item in files2rem:
                 print ("removing file: %s"%item)
                 try:os.unlink(item)#pass#
@@ -119,34 +129,69 @@ def IterateTraining (nrIter, varT, nrIterStable):
         
         
         if score == 0:                  
-            shutil.copy("%s.state.iter.%03d.block.all"%(paramDict["BlockFilePrefix"],it), "%s.state.vart.%05.05f.all"%(paramDict["BlockFilePrefix"],varT))
+            shutil.copy("%s.state.iter.%04d.block.all"%(paramDict["BlockFilePrefix"],it), "%s.state.vart.%05.05f.all"%(paramDict["BlockFilePrefix"],varT))
             print ("yyeeeeeyyyy")
-            return True
+            retValue = True
         elif it > nrIterStable:
             if min(score_vect[0:len(score_vect)-nrIterStable]) - min(score_vect[-nrIterStable:]) < 0.001:
-                return False
-    return False
+                break
+    
+    lastIt += 1
+    ExecuteCommand("BlockScoreComputation", lastIt) 
+    ExecuteCommand("LastBlockTraining", lastIt) 
+    ExecuteCommand("GatherBlockStates", lastIt) 
+    
+    obj = open("%s.state.iter.%04d.block.score"%(paramDict["BlockFilePrefix"],lastIt))
+    score = float(obj.read().strip())
+    obj.close()        
+
+    lastIt -= 1    
+    
+    if score < 0.0001 :
+        shutil.copy("%s.state.iter.%04d.block.all"%(paramDict["BlockFilePrefix"],lastIt), "%s.state.iter.%04d.block.all"%(paramDict["BlockFilePrefix"], 0))
+        retValue = True
+    else:
+        retValue = False    
+            
+    files2rem = glob.glob("%s.state.iter.%04d.block.????"%(paramDict["BlockFilePrefix"],lastIt))
+    files2rem += glob.glob("%s.state.iter.%04d.block.last"%(paramDict["BlockFilePrefix"],lastIt))
+    files2rem += glob.glob("%s.state.iter.%04d.block.score"%(paramDict["BlockFilePrefix"],lastIt))
+    files2rem += glob.glob("%s.state.iter.%04d.block.all"%(paramDict["BlockFilePrefix"],lastIt))    
+    
+    lastIt += 1
+    files2rem += glob.glob("%s.state.iter.%04d.block.????"%(paramDict["BlockFilePrefix"],lastIt))
+    files2rem += glob.glob("%s.state.iter.%04d.block.last"%(paramDict["BlockFilePrefix"],lastIt))
+    files2rem += glob.glob("%s.state.iter.%04d.block.score"%(paramDict["BlockFilePrefix"],lastIt))
+    files2rem += glob.glob("%s.state.iter.%04d.block.all"%(paramDict["BlockFilePrefix"],lastIt))    
+    
+    for item in files2rem:
+        print ("removing file: %s"%item)
+        try:os.unlink(item)#pass#
+        except: pass       
+        
+    return retValue
     
 
 def BinarySearchTraining ():
     varTLeft  = 0.0
     varTRight = 0.0
     epsilon = 0.001
-    maxNrIter = 1000
+    maxNrIter = 4000
     nrIterStable = 50
     
     foundFeasible = False
     
     for pow2 in range (0, 10):
         varTLeft = varTRight
-        varTRight = 2**pow2
+        varTRight = 2**pow2 + 1
         if IterateTraining(maxNrIter, varTRight, nrIterStable):
-            foundFeasible = True
-            exit(1)
+            foundFeasible = True            
             break
     if not foundFeasible:
         print ("cannot perform training on this database. sorry")
         exit(1)
+        
+    nrIterStable *= 3
     
     while varTRight-varTLeft > epsilon:
         varTCenter = (varTLeft + varTRight)/2
@@ -168,12 +213,16 @@ def PerformClassification():
     mydict["ConnectorTest"] =  testCon        
     
     mydict["Command"] = "Clasify"
-    mydict["ModelFile"] = "bitdefender.state.vart.00000.all"
+    mydict["ModelFile"] = "pvm.state.iter.0000.block.all"
     
     gmlpy.Run(mydict)
     
 if __name__ == "__main__":
+    trainDataSet = "breast-cancer-wisconsin.data.fullcache"
+    testDataSet = "breast-cancer-wisconsin.data.fullcache"
+    connectorType = "FullCacheConnector"
     MakeInitialPrep()    
-    #BinarySearchTraining()
-    #PerformClassification()
-    IterateTraining(500, 1.0, 50);
+    BinarySearchTraining()
+
+    PerformClassification()
+    #IterateTraining(4000, 1.0, 100);
